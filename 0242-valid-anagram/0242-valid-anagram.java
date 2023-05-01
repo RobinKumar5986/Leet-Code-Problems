16de42dfef816b9ca3ab69f3f196733c7aa95362
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-05-01 12:00:50 +0530

    Time: 243 ms (5.07%), Space: 43.8 MB (5.48%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+//---------Brute Force Using Mapping---------//
+        if(s.length()!=t.length() || s.length()<1) return false;
+        HashMap<Character,Integer> map1=new HashMap<>();
+        HashMap<Character,Integer> map2=new HashMap<>();
+        //-------Mapping--------//
+        for(int i=0;i<s.length();i++){
+            char c1=s.charAt(i);
+            char c2=t.charAt(i);
+            if(map1.containsKey(c1)){
+                int temp=map1.get(c1)+1;
+                map1.put(c1,temp);
+            }else{
+                map1.put(c1,1);
+            }
+            if(map2.containsKey(c2)){
+                int temp=map2.get(c2)+1;
+                map2.put(c2,temp);
+            }else{
+                map2.put(c2,1);
+            }
+        }
+        //--------Matching-------//
+        for(int i=0;i<s.length();i++){
+            char c1=s.charAt(i);
+            char c2=t.charAt(i);
+            if(map1.containsKey(c2)){
+                int n1=map1.get(c2);
+                int n2=map2.get(c2);
+                System.out.println("n1 : "+n1+" n2 : "+n2);
+                if(n1!=n2) return false;
+            }else{
+                return false;
+            }
+            if(map2.containsKey(c1)){
+                int n1=map1.get(c1);
+                int n2=map2.get(c1);
+                System.out.println("n1 : "+n1+" n2 : "+n2);
+                if(n1!=n2) return false;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
