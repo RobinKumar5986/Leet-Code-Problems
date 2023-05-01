c6b1192ea4a9d49659061e1d9042012f8ec46419
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-05-01 22:25:54 +0530

    Time: 12 ms (5.29%), Space: 43.4 MB (5.34%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -10,28 +10,19 @@ class Solution {
             int n2=str[i].length();
             int min=(n1<n2) ? n1 :n2;
             String temp="";
-
-
             for(int j=0;j<min;j++){
                 char c1=s.charAt(j);
                 char c2=str[i].charAt(j);
-                System.out.println("c1 : " + c1 );
-                System.out.println("c2 : " + c2);
                 if(c1==c2) temp+=c1;
                 else{
                     break;
                 }
             }
 
-
-            // System.out.println("temp : " + temp );
-            // System.out.println("Pre FIX : " + s);
             if(temp.length()<=n1){
                 s=temp;
             }
         }
-        // System.out.println("Pre FIX : " + s);
-
         return s;
     }
 }
