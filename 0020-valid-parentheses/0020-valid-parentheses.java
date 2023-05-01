8c3fd5e80706c0e010a409453ee65221ea268cfe
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-05-01 13:04:49 +0530

    Time: 12 ms (13.85%), Space: 41.1 MB (5.18%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,45 @@
+class Solution {
+//------------implementation form Scratch------------//
+    public void push(char val,char ar[],int top){
+        ar[top]=val;
+    }
+    public char pop(int top,char ar[]){
+        return ar[top];
+    }
+//-------------------------------------------------//
+    public boolean isValid(String s) {
+        if(s.length()%2!=0) return false;
+
+        int top=-1;
+        Solution sol=new Solution();
+
+        char[] ar=new char[s.length()];
+
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(c=='(' || c=='{' || c=='['){
+                if(c=='('){
+                    c=')';
+                }else if(c=='{'){
+                    c='}';
+                }else{
+                    c=']';
+                }
+                top+=1;
+                sol.push(c,ar,top);
+                System.out.println(ar[top]);
+            }else{
+                if(top<0) return false;
+                char c2=sol.pop(top,ar);
+                top-=1;
+
+                if(c!=c2) return false;
+            }
+        }
+        System.out.println("TOP : "+top);
+        if(top!=-1) return false;
+
+        return true;
+
+    }
+}
