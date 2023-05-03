2704eb2d8db827bda05c2475adabef7edd098aa7
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-05-03 12:30:25 +0530

    Time: 5 ms (97.54%), Space: 42.4 MB (68.67%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,30 +1,56 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int l=0;
-        int r=0;
-        int len=0;
-        int wind=0;
-        int freq=0;
+        // int l=0;
+        // int r=0;
+        // int len=0;
+        // int wind=0;
+        // int freq=0;
+
+        // int n=s.length();
+
+        // HashMap<Character,Integer> map=new HashMap<>();
+
+        // while(r<n){
+        //     char c=s.charAt(r);
+        //     wind=r-l+1;
+        //     map.put(c,map.getOrDefault(c,0)+1);
+        //     if(map.get(c)>freq){
+        //         freq=map.get(c);
+        //     }
+        //     int temp=wind-freq;
+        //     if(temp<=k){
+        //         len=wind;
+        //     }else{
+        //         map.put(s.charAt(l),map.get(s.charAt(l))-1);
+        //         l++;
+        //     }
+        //     r++;
+        // }
+        // return len;
+
+//-----------More Optmized Solution----------//
 
         int n=s.length();
+        int len=0;
+        int wind=0;
+        int l=0;
+        int freq=0;
+        //mapping
+        int[] map=new int[26];
 
-        HashMap<Character,Integer> map=new HashMap<>();
-
-        while(r<n){
+        for(int r=0;r<n;r++){
             char c=s.charAt(r);
-            wind=r-l+1;
-            map.put(c,map.getOrDefault(c,0)+1);
-            if(map.get(c)>freq){
-                freq=map.get(c);
-            }
-            int temp=wind-freq;
-            if(temp<=k){
-                len=wind;
-            }else{
-                map.put(s.charAt(l),map.get(s.charAt(l))-1);
+            map[c-'A']+=1;          //we can also write as map[c-'A']++;
+            int temp=map[c-'A'];        //frequency of current element
+            freq=Math.max(freq,temp);
+
+            while(r - l + 1 -freq > k)  //if the overall frequency of the element is grater the the No. of allowed step 'k' just POP
+            {
+                map[s.charAt(l)-'A']--;
                 l++;
             }
-            r++;
+            
+            len=Math.max(len,r-l+1); //setting the max length r - l + 1 is the window size 
         }
         return len;
 
