3932de0cee07054b96726266d8a562128014a780
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-05-05 00:31:46 +0530

    Time: 5 ms (53.55%), Space: 40.6 MB (68.38%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -4,19 +4,20 @@ class Solution {
         if(s.length()<=0) return 0;
         int c=0;
         for(int i=0;i<s.length();i++){
-            int len1=expand(s,i,i+1);
-            int len2=expand(s,i,i);
-            c+=len1+len2;
-        }
-        return c;
-
-    }
-    public int expand(String s,int i,int j){
-        int c=0;
-        while(i>=0 && j<s.length() && s.charAt(i)==s.charAt(j)){
-            i--;
-            j++;
-            c++;
+            int x=i;
+            int y=i;
+            while(x>=0 && y<s.length() && s.charAt(x)==s.charAt(y)){
+                x--;
+                y++;
+                c++;
+            }
+            x=i;
+            y=i+1;
+            while(x>=0 && y<s.length() && s.charAt(x)==s.charAt(y)){
+                x--;
+                y++;
+                c++;
+            }
         }
         return c;
     }
