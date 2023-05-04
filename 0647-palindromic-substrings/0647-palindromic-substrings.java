0d9739646ef578a02cb6b34faf1b38fd5a13c611
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-05-05 00:26:59 +0530

    Time: 1 ms (99.86%), Space: 40.6 MB (75.33%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,27 +1,41 @@
+// class Solution {
+//     public int countSubstrings(String s) {
+//         //------solution------//
+//         if(s.length()<=0) return 0;
+
+//         int start=0;
+//         int end=0;
+//         int c=0;
+//         for(int i=0;i<s.length();i++){
+//             int len1=expand(s,i,i+1);
+//             int len2=expand(s,i,i);
+//             c+=len1+len2;
+//         }
+//         return c;
+
+//     }
+//     public int expand(String s,int i,int j){
+//         int c=0;
+//         while(i>=0 && j<s.length() && s.charAt(i)==s.charAt(j)){
+//             i--;
+//             j++;
+
+//            c++;
+//         }
+//         return c;
+//     }
+// }
+
 class Solution {
-    public int countSubstrings(String s) {
-        //------solution------//
-        if(s.length()<=0) return 0;
-
-        int start=0;
-        int end=0;
-        int c=0;
-        for(int i=0;i<s.length();i++){
-            int len1=expand(s,i,i+1);
-            int len2=expand(s,i,i);
-            c+=len1+len2;
+    public int countSubstrings(String S) {
+        int len = S.length(), ans = 0;
+        for (int i = 0; i < len; i++) {
+            int j = i - 1, k = i;
+            while (k < len - 1 && S.charAt(k) == S.charAt(k+1)) k++;
+            ans += (k - j) * (k - j + 1) / 2;
+            i = k++;
+            while (j >= 0 && k < len && S.charAt(k++) == S.charAt(j--)) ans++;
         }
-        return c;
-
-    }
-    public int expand(String s,int i,int j){
-        int c=0;
-        while(i>=0 && j<s.length() && s.charAt(i)==s.charAt(j)){
-            i--;
-            j++;
-
-           c++;
-        }
-        return c;
+        return ans;
     }
 }
