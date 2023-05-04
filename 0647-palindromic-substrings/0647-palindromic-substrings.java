02acdd4331d2f72708a4b8a2bb5b1267d3c21400
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-05-05 00:22:33 +0530

    Time: 4 ms (73.1%), Space: 40.5 MB (75.33%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int countSubstrings(String s) {
+        //------solution------//
+        if(s.length()<=0) return 0;
+
+        int start=0;
+        int end=0;
+        int c=0;
+        for(int i=0;i<s.length();i++){
+            int len1=expand(s,i,i+1);
+            int len2=expand(s,i,i);
+            // int len=Math.max(len1,len2);
+            c+=len1+len2;
+        }
+        return c;
+
+    }
+    public int expand(String s,int i,int j){
+        int c=0;
+        while(i>=0 && j<s.length() && s.charAt(i)==s.charAt(j)){
+            i--;
+            j++;
+
+           c++;
+        }
+        return c;
+    }
+}
