4e0d0f743be650a8d38ea941bf0db7a8b497aab7
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-05-05 17:44:35 +0530

    Time: 1 ms (76.94%), Space: 40.9 MB (43.26%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,28 +1,46 @@
 class Solution {
-    public String longestCommonPrefix(String[] str) {
-        if(str.length<=0) return "";
+    public String longestCommonPrefix(String[] strs) {
+//------------Brute Force----------------//
+        // if(str.length<=0) return "";
 
-        String s=str[0];
+        // String s=str[0];
 
-        for(int i=1;i<str.length;i++){
+        // for(int i=1;i<str.length;i++){
 
-            int n1=s.length();
-            int n2=str[i].length();
-            int min=(n1<n2) ? n1 :n2;
-            String temp="";
-            for(int j=0;j<min;j++){
-                char c1=s.charAt(j);
-                char c2=str[i].charAt(j);
-                if(c1==c2) temp+=c1;
-                else{
-                    break;
-                }
-            }
+        //     int n1=s.length();
+        //     int n2=str[i].length();
+        //     int min=(n1<n2) ? n1 :n2;
+        //     String temp="";
+        //     for(int j=0;j<min;j++){
+        //         char c1=s.charAt(j);
+        //         char c2=str[i].charAt(j);
+        //         if(c1==c2) temp+=c1;
+        //         else{
+        //             break;
+        //         }
+        //     }
 
-            if(temp.length()<=n1){
-                s=temp;
-            }
+        //     if(temp.length()<=n1){
+        //         s=temp;
+        //     }
+        // }
+        // return s;
+
+//--------Optmizeed Solution---------//
+       if (strs == null || strs.length == 0)
+            return "";
+        
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+        int c = 0;
+        while(c < first.length())
+        {
+            if (first.charAt(c) == last.charAt(c))
+                c++;
+            else
+                break;
         }
-        return s;
+        return c == 0 ? "" : first.substring(0, c);
     }
 }
