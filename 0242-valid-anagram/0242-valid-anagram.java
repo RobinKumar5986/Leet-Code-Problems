1b788606985d68b335aad9ff7281bda555986193
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-05-14 16:12:54 +0530

    Time: 25 ms (10.22%), Space: 42.9 MB (41.3%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -10,56 +10,11 @@ class Solution {
 
             map1.put(c1,map1.getOrDefault(c1,0)+1);
             map1.put(c2,map1.getOrDefault(c2,0)-1);
-            
-            // if(map1.containsKey(c1)){
-            //     int temp=map1.get(c1)+1;
-            //     map1.put(c1,temp);
-            // }else{
-            //     map1.put(c1,1);
-            // }
-            // if(map2.containsKey(c2)){
-            //     int temp=map2.get(c2)+1;
-            //     map2.put(c2,temp);
-            // }else{
-            //     map2.put(c2,1);
-            // }
         }
         //--------Matching-------//
         for(int val : map1.values()){
             if(val!=0) return false;
-            // char c1=s.charAt(i);
-            // char c2=t.charAt(i);
-            // if(map1.containsKey(c2)){
-            //     int n1=map1.get(c2);
-            //     int n2=map2.get(c2);
-            //     System.out.println("n1 : "+n1+" n2 : "+n2);
-            //     if(n1!=n2) return false;
-            // }else{
-            //     return false;
-            // }
-            // if(map2.containsKey(c1)){
-            //     int n1=map1.get(c1);
-            //     int n2=map2.get(c1);
-            //     System.out.println("n1 : "+n1+" n2 : "+n2);
-            //     if(n1!=n2) return false;
-            // }else{
-            //     return false;
-            // }
         }
         return true;
-
-// //------------optmized Solution------------//
-//         if(s.length()!=t.length() || s.length()<1) return false;
-
-//         int alpha[]=new int[26];
-//         int n=s.length();
-//         for(int i=0; i < n; i++){
-//             alpha[s.charAt(i) - 'a']++;
-//             alpha[t.charAt(i) - 'a']--;
-//         }
-//         for(int i=0;i<alpha.length;i++){
-//             if(alpha[i]!=0) return false;
-//         }
-//         return true;
     }
 }
