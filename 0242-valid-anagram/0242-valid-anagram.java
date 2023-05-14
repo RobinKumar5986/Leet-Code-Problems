b0530991397020e0f33e24aa08e090070a05a594
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-05-14 16:12:10 +0530

    Time: 26 ms (9.3%), Space: 42.6 MB (81.93%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,61 +1,65 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 //---------Brute Force Using Mapping---------//
-    //     if(s.length()!=t.length() || s.length()<1) return false;
-    //     HashMap<Character,Integer> map1=new HashMap<>();
-    //     HashMap<Character,Integer> map2=new HashMap<>();
-    //     //-------Mapping--------//
-    //     for(int i=0;i<s.length();i++){
-    //         char c1=s.charAt(i);
-    //         char c2=t.charAt(i);
-    //         if(map1.containsKey(c1)){
-    //             int temp=map1.get(c1)+1;
-    //             map1.put(c1,temp);
-    //         }else{
-    //             map1.put(c1,1);
-    //         }
-    //         if(map2.containsKey(c2)){
-    //             int temp=map2.get(c2)+1;
-    //             map2.put(c2,temp);
-    //         }else{
-    //             map2.put(c2,1);
-    //         }
-    //     }
-    //     //--------Matching-------//
-    //     for(int i=0;i<s.length();i++){
-    //         char c1=s.charAt(i);
-    //         char c2=t.charAt(i);
-    //         if(map1.containsKey(c2)){
-    //             int n1=map1.get(c2);
-    //             int n2=map2.get(c2);
-    //             System.out.println("n1 : "+n1+" n2 : "+n2);
-    //             if(n1!=n2) return false;
-    //         }else{
-    //             return false;
-    //         }
-    //         if(map2.containsKey(c1)){
-    //             int n1=map1.get(c1);
-    //             int n2=map2.get(c1);
-    //             System.out.println("n1 : "+n1+" n2 : "+n2);
-    //             if(n1!=n2) return false;
-    //         }else{
-    //             return false;
-    //         }
-    //     }
-    //     return true;
-
-//------------optmized Solution------------//
         if(s.length()!=t.length() || s.length()<1) return false;
+        HashMap<Character,Integer> map1=new HashMap<>();
+        //-------Mapping--------//
+        for(int i=0;i<s.length();i++){
+            char c1=s.charAt(i);
+            char c2=t.charAt(i);
 
-        int alpha[]=new int[26];
-        int n=s.length();
-        for(int i=0; i < n; i++){
-            alpha[s.charAt(i) - 'a']++;
-            alpha[t.charAt(i) - 'a']--;
+            map1.put(c1,map1.getOrDefault(c1,0)+1);
+            map1.put(c2,map1.getOrDefault(c2,0)-1);
+            
+            // if(map1.containsKey(c1)){
+            //     int temp=map1.get(c1)+1;
+            //     map1.put(c1,temp);
+            // }else{
+            //     map1.put(c1,1);
+            // }
+            // if(map2.containsKey(c2)){
+            //     int temp=map2.get(c2)+1;
+            //     map2.put(c2,temp);
+            // }else{
+            //     map2.put(c2,1);
+            // }
         }
-        for(int i=0;i<alpha.length;i++){
-            if(alpha[i]!=0) return false;
+        //--------Matching-------//
+        for(int val : map1.values()){
+            if(val!=0) return false;
+            // char c1=s.charAt(i);
+            // char c2=t.charAt(i);
+            // if(map1.containsKey(c2)){
+            //     int n1=map1.get(c2);
+            //     int n2=map2.get(c2);
+            //     System.out.println("n1 : "+n1+" n2 : "+n2);
+            //     if(n1!=n2) return false;
+            // }else{
+            //     return false;
+            // }
+            // if(map2.containsKey(c1)){
+            //     int n1=map1.get(c1);
+            //     int n2=map2.get(c1);
+            //     System.out.println("n1 : "+n1+" n2 : "+n2);
+            //     if(n1!=n2) return false;
+            // }else{
+            //     return false;
+            // }
         }
         return true;
+
+// //------------optmized Solution------------//
+//         if(s.length()!=t.length() || s.length()<1) return false;
+
+//         int alpha[]=new int[26];
+//         int n=s.length();
+//         for(int i=0; i < n; i++){
+//             alpha[s.charAt(i) - 'a']++;
+//             alpha[t.charAt(i) - 'a']--;
+//         }
+//         for(int i=0;i<alpha.length;i++){
+//             if(alpha[i]!=0) return false;
+//         }
+//         return true;
     }
 }
