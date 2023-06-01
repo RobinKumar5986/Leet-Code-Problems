337eeefcbcc0acff681cdcaa493cca8be14991d7
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-06-02 02:04:10 +0530

    Time: 7 ms (76.45%), Space: 46.7 MB (38.62%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,31 +1,48 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-//--------optmized solution----------//
-        HashMap<HashMap<Character,Integer> ,ArrayList<String>> bmap=new HashMap<>();
-
-
-        for(String str : strs ){
-            HashMap<Character,Integer> fmap=new HashMap<>();
+//------------Solution---------------------//
+        // HashMap<HashMap<Character,Integer> ,ArrayList<String>> bmap=new HashMap<>();
+        // for(String str : strs ){
+        //     HashMap<Character,Integer> fmap=new HashMap<>();
         
-            for(int i=0;i<str.length();i++){
-                char c=str.charAt(i);
-                fmap.put(c,fmap.getOrDefault(c,0) + 1);
-            }
+        //     for(int i=0;i<str.length();i++){
+        //         char c=str.charAt(i);
+        //         fmap.put(c,fmap.getOrDefault(c,0) + 1);
+        //     }
 
-            if(! bmap.containsKey(fmap)){
-                ArrayList<String> list=new ArrayList<>();
-                list.add(str);
-                bmap.put(fmap,list);
+        //     if(! bmap.containsKey(fmap)){
+        //         ArrayList<String> list=new ArrayList<>();
+        //         list.add(str);
+        //         bmap.put(fmap,list);
+        //     }else{
+        //         ArrayList<String> list=bmap.get(fmap);
+        //         list.add(str);
+        //     }
+        // }
+        // List<List<String>> result=new ArrayList<>();
+        // for(ArrayList<String> val : bmap.values()){
+        //     result.add(val);
+        // }
+        // return result;
+//-------------more optmizes solution-----------//
+    HashMap<String,List<String>> map=new HashMap<>();    
+        for(int i=0;i<strs.length;i++){
+            String s1=strs[i];
+            char[] arr=s1.toCharArray();
+
+            Arrays.sort(arr);
+            String str=new String(arr);
+
+            if(map.containsKey(str)){
+
+                map.get(str).add(s1);
+                
             }else{
-                ArrayList<String> list=bmap.get(fmap);
-                list.add(str);
+                map.put(str,new ArrayList<>());
+                map.get(str).add(s1);
             }
         }
-        List<List<String>> result=new ArrayList<>();
-        for(ArrayList<String> val : bmap.values()){
-            result.add(val);
-        }
-        return result;
+        return new ArrayList<>(map.values());
 
 
     }
