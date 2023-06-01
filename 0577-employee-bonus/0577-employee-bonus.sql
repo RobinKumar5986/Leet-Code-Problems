4a26d19934345fec9f479a95d3f9ab9096544115
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-06-02 01:58:00 +0530

    Time: 2270 ms (13.34%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,5 +1,9 @@
 # Write your MySQL query statement below
+# select e.name ,b.bonus from Employee e
+# left join Bonus b
+# on e.empId=b.empId 
+# where COALESCE(b.bonus,0)<1000;
 select e.name ,b.bonus from Employee e
 left join Bonus b
 on e.empId=b.empId 
-where COALESCE(b.bonus,0)<1000;
+where IFNULL(b.bonus,0)<1000
