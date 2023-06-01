e4822cba13f106c674337b9e7134df8d5e0d5c50
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-06-02 02:03:58 +0530

    Time: 54 ms (40.41%), Space: 43.8 MB (23.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -16,38 +16,7 @@ class Solution {
             }
         }
 
-        // int x[]=new int[nums.length];
-        // for(int i=0;i<nums.length;i++){
-        //     x[i]=nums[i];
-        // }
-        // Arrays.sort(nums);
-
-        // int low=0;
-        // int up=nums.length-1;
-        // while(low<up){
-        //     int sum=nums[low]+nums[up];
-        //     if(sum==target){
-        //         int ans[]=new int[2];
-        //         int flag=0;
-        //         for(int i=0;i<x.length;i++){
-        //             if(x[i]==nums[low]&&flag!=1){
-        //                 ans[0]=i;
-        //                 flag++;
-        //             }
-        //             else if(x[i]==nums[up]){
-        //                 ans[1]=i;
-        //                 flag++;
-        //             }
-        //             if(flag==2){
-        //                 return ans;
-        //             }
-        //         }
-        //     }else if(sum<target){
-        //         low++;
-        //     }else{
-        //         up--;
-        //     }
-        // }
+        
         return null;
     }
 }
