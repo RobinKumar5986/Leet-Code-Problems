71a174a96d210e886558465464a419ba66609b90
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-06-07 19:11:25 +0530

    Time: 6 ms (85.68%), Space: 44 MB (15.27%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -23,23 +23,40 @@ class Solution {
         // }
         // return len;
 
-        HashMap<Character,Integer> map=new HashMap<>();
-        int n=s.length();
-        int r=0;
-        int l=0;
-        int len=0;
+        // HashMap<Character,Integer> map=new HashMap<>();
+        // int n=s.length();
+        // int r=0;
+        // int l=0;
+        // int len=0;
 
-        while(r<n){
+        // while(r<n){
+        //     char c=s.charAt(r);
+        //     if(map.containsKey(c)){
+        //         l=Math.max(l,map.get(c)+1);
+        //     }
+        //     map.put(c,r);
+        //     len=Math.max(len,r-l+1);
+        //     r++;
+        // }
+        // return len;
+
+
+        HashMap<Character,Integer> map=new HashMap<>();
+        int l=0;
+        int r=0;
+        int len=0;
+        while(r<s.length()){
             char c=s.charAt(r);
-            if(map.containsKey(c)){
-                l=Math.max(l,map.get(c)+1);
+
+            if( map.containsKey(c) ){
+                 l=Math.max(l,map.get(c)+1);
             }
-            map.put(c,r);
             len=Math.max(len,r-l+1);
+            map.put(c,r);
             r++;
+        
         }
 
         return len;
-
     }
 }
