8e8c32b5fe80cf855df7ba7d56a43727e338710e
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-06-08 22:29:26 +0530

    Time: 6 ms (85.84%), Space: 43.6 MB (29.67%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -23,40 +23,40 @@ class Solution {
         // }
         // return len;
 
-        // HashMap<Character,Integer> map=new HashMap<>();
-        // int n=s.length();
-        // int r=0;
-        // int l=0;
-        // int len=0;
+        HashMap<Character,Integer> map=new HashMap<>();
+        int n=s.length();
+        int r=0;
+        int l=0;
+        int len=0;
 
-        // while(r<n){
-        //     char c=s.charAt(r);
-        //     if(map.containsKey(c)){
-        //         l=Math.max(l,map.get(c)+1);
-        //     }
-        //     map.put(c,r);
-        //     len=Math.max(len,r-l+1);
-        //     r++;
-        // }
-        // return len;
+        while(r<n){
+            char c=s.charAt(r);
+            if(map.containsKey(c)){
+                l=Math.max(l,map.get(c)+1);
+            }
+            map.put(c,r);
+            len=Math.max(len,r-l+1);
+            r++;
+        }
+        return len;
 
         //--------Using array because it is faster then Hash map-------//
         
-        int[] map=new int[256];
-        int l=0;
-        int r=0;
-        int len=0;
-        while(r<s.length()){
-            char c=s.charAt(r);
-            if(map[c]!=0){
-                l=Math.max(l,map[c]);
-            }
-            len=Math.max(len,r-l+1);
-            map[c]=r+1;
-            r++;
-        }
+        // int[] map=new int[256];
+        // int l=0;
+        // int r=0;
+        // int len=0;
+        // while(r<s.length()){
+        //     char c=s.charAt(r);
+        //     if(map[c]!=0){
+        //         l=Math.max(l,map[c]);
+        //     }
+        //     len=Math.max(len,r-l+1);
+        //     map[c]=r+1;
+        //     r++;
+        // }
 
-        return len;
+        // return len;
 
     
     }
