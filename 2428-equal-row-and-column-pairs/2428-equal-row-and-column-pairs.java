00691daf76b7a15c8604668102c4b09b22e327f1
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-06-13 22:49:50 +0530

    Time: 48 ms (54.17%), Space: 49 MB (73.96%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
@@ -1,34 +1,45 @@
 class Solution {
     public int equalPairs(int[][] grid) {
-        List<String> row = new ArrayList<>();
-        HashMap<String,Integer> col = new HashMap<>();
+        // List<String> row = new ArrayList<>();
+        // HashMap<String,Integer> col = new HashMap<>();
+        // int count=0;
+
+        // for(int i=0;i<grid.length;i++){
+        //     String r="";
+        //     String c="";
+        //     for(int j=0;j<grid.length;j++){
+        //         r=r+grid[i][j]+",";
+        //         c=c+grid[j][i]+",";
+        //     }            
+        //     row.add(r);
+        //     if(!col.containsKey(c)){
+        //         col.put(c,1);
+        //     }else{
+        //         col.put(c,col.get(c)+1);
+        //     }
+            
+        // }
+        // for(int i=0;i<row.size();i++){
+        //     if(col.containsKey(row.get(i))){
+        //         count+=col.get(row.get(i));
+        //     }
+        // }
+        // return  count;
+
+//-----------Optmized code------------//
+        Map<String,Integer> map=new HashMap<>();
+
+        for(int[] row : grid){
+            map.merge(Arrays.toString(row),1,Integer::sum);
+        }
         int count=0;
-
-        for(int i=0;i<grid.length;i++){
-            String r="";
-            String c="";
-            for(int j=0;j<grid.length;j++){
-                r=r+grid[i][j]+",";
-                c=c+grid[j][i]+",";
+        for(int c=0,n=grid.length; c<n;++c){
+            int[] col=new int[n];
+            for (int r = 0; r < n; ++r) {
+                col[r] = grid[r][c];
             }
-            System.out.println("Row : "+r);
-            System.out.println("Col : "+c);
-            
-            row.add(r);
-             
-            if(!col.containsKey(c)){
-                col.put(c,1);
-            }else{
-                col.put(c,col.get(c)+1);
-            }
-            
+            count += map.getOrDefault(Arrays.toString(col), 0);
         }
-        for(int i=0;i<row.size();i++){
-            if(col.containsKey(row.get(i))){
-                count+=col.get(row.get(i));
-            }
-        }
-        return  count;
-
+        return count;
     }
 }
