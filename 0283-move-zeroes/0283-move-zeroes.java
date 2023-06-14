ecdfc97dd1bc7ffcebedf0b2af073df67fc0d3cb
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-06-14 23:59:36 +0530

    Time: 2 ms (42.25%), Space: 45.4 MB (9.61%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,29 +1,40 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        // Arrays.sort(nums);
-       int zc=0;
-       int zind=0;
-       for(int i=0;i<nums.length;i++){
-           if(zc==0 && nums[i]==0){
-               zc++;
-               zind=i;
-           }
-           if(zc!=0 && nums[i]==0){
-               zc++;
-           }
+    //    int zc=0;
+    //    int zind=0;
+    //    for(int i=0;i<nums.length;i++){
+    //        if(zc==0 && nums[i]==0){
+    //            zc++;
+    //            zind=i;
+    //        }
+    //        if(zc!=0 && nums[i]==0){
+    //            zc++;
+    //        }
+    //        if(nums[i]!=0 && zc >=1){
+    //            nums[zind]=nums[i];
+    //            nums[i]=0;
+    //            if(zc>1){
+    //                zind++;
+    //            }
+    //            else{
+    //                zind=i;
+    //            }
+    //        }
+    //    }
+    // }class Solution {
 
-           if(nums[i]!=0 && zc >=1){
-               nums[zind]=nums[i];
-               nums[i]=0;
+//-------Opmized Solution-------------//
 
-
-               if(zc>1){
-                   zind++;
-               }
-               else{
-                   zind=i;
-               }
-           }
-       }
+        int snowBallSize = 0; 
+        for (int i=0;i<nums.length;i++){
+	        if (nums[i]==0){
+                snowBallSize++; 
+            }
+            else if (snowBallSize > 0) {
+	            int t = nums[i];
+	            nums[i]=0;
+	            nums[i-snowBallSize]=t;
+            }
+        }
     }
 }
