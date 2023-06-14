d6a1bc861a036e711c218368639d885dabf754d6
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-06-14 23:51:14 +0530

    Time: 2 ms (42.25%), Space: 45.3 MB (25.27%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,16 +1,30 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         // Arrays.sort(nums);
-        int temp[]=new int[nums.length];
-        int j=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0){
-                temp[j]=nums[i];
-                j++;
-            }
-        }
-        for(int i=0;i<nums.length;i++){
-            nums[i]=temp[i];
-        }
+       int zc=0;
+       int zind=0;
+       for(int i=0;i<nums.length;i++){
+           if(zc==0 && nums[i]==0){
+               zc++;
+               zind=i;
+           }
+           if(zc!=0 && nums[i]==0){
+               zc++;
+           }
+
+           if(nums[i]!=0 && zc >=1){
+               nums[zind]=nums[i];
+               nums[i]=0;
+
+
+               if(zc>1){
+                   zind++;
+                //    zc--;
+               }
+               else{
+                   zind=i;
+               }
+           }
+       }
     }
 }
