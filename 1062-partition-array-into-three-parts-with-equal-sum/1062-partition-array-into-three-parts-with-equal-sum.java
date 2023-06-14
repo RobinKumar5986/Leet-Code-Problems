01ab1b8c850849346a8f8969cc89668ed394b2a3
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-06-14 23:26:00 +0530

    Time: 10 ms (5.63%), Space: 54.5 MB (5.91%) - LeetHub

--- 1062-partition-array-into-three-parts-with-equal-sum/1062-partition-array-into-three-parts-with-equal-sum.java
+++ 1062-partition-array-into-three-parts-with-equal-sum/1062-partition-array-into-three-parts-with-equal-sum.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean canThreePartsEqualSum(int[] arr) {
-        int sum=0;
+        Integer sum = Arrays.stream(arr).reduce(0, Integer::sum);
         int count=0;
 
-        for(int i=0;i<arr.length;i++){
-            sum+=arr[i];
-        }
+        // for(int i=0;i<arr.length;i++){
+        //     sum+=arr[i];
+        // }
         //First base case 
         if(sum%3!=0) return false;
         int crSum=sum/3;
