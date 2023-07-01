18cf0da7ddd6cc1d4fa9454c5b4bc77b3411ab5f
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-07-01 16:39:06 +0530

    Time: 1 ms (100%), Space: 44.9 MB (55.22%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -1,39 +1,35 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int square[]=new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            square[i]=nums[i]*nums[i];
-        }
-        Arrays.sort(square);
-        return square;
+        // int square[]=new int[nums.length];
+        // for(int i=0;i<nums.length;i++){
+        //     square[i]=nums[i]*nums[i];
+        // }
+        // Arrays.sort(square);
+        // return square;
     // }
 
     //----two pointer Solution-----//
-        // int lo=0;
-        // int hi=nums.length-1;
-        // int result[]=new int[nums.length];
-        // int ind=nums.length-1;
+        int hi=nums.length-1;
+        int lo=0;
+        int[] ans=new int[nums.length];
+        int k=ans.length-1;
 
-        // int num1=nums[lo]*nums[lo];
-        // int num2=nums[hi]*nums[hi];
+        while(lo<=hi){
+            int s1=nums[lo]*nums[lo];
+            int s2=nums[hi]*nums[hi];
 
-        // while(lo<=hi){
-        //    if(num1<=num2)
-        //    {
-        //        result[ind]=num2;
-        //        hi--;
-        //        if(hi>=0)
-        //         num2=nums[hi]*nums[hi];
+            if(s2>s1){
+                ans[k]=s2;
+                hi--;
 
-        //    }else{
-        //        result[ind]=num1;
-        //        if(lo<=nums.length-1)
-        //             lo++;
-        //        num1=nums[lo]*nums[lo];
-        //    }
-        //    ind--;
-           
-        // }
-        // return result;
+            }else{
+                ans[k]=s1;
+                lo++;
+            }
+            k--;
+
+        }
+        return ans;
+
     }
 }
