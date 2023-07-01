3b2de1fe55360cd00d7a0d9be074e3b96653ae52
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-07-01 16:22:38 +0530

    Time: 5 ms (47.78%), Space: 45.6 MB (5.15%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -1,39 +1,39 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-    //     int square[]=new int[nums.length];
-    //     for(int i=0;i<nums.length;i++){
-    //         square[i]=nums[i]*nums[i];
-    //     }
-    //     Arrays.sort(square);
-    //     return square;
+        int square[]=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            square[i]=nums[i]*nums[i];
+        }
+        Arrays.sort(square);
+        return square;
     // }
 
     //----two pointer Solution-----//
-        int lo=0;
-        int hi=nums.length-1;
-        int result[]=new int[nums.length];
-        int ind=nums.length-1;
+        // int lo=0;
+        // int hi=nums.length-1;
+        // int result[]=new int[nums.length];
+        // int ind=nums.length-1;
 
-        int num1=nums[lo]*nums[lo];
-        int num2=nums[hi]*nums[hi];
+        // int num1=nums[lo]*nums[lo];
+        // int num2=nums[hi]*nums[hi];
 
-        while(lo<=hi){
-           if(num1<=num2)
-           {
-               result[ind]=num2;
-               hi--;
-               if(hi>=0)
-                num2=nums[hi]*nums[hi];
+        // while(lo<=hi){
+        //    if(num1<=num2)
+        //    {
+        //        result[ind]=num2;
+        //        hi--;
+        //        if(hi>=0)
+        //         num2=nums[hi]*nums[hi];
 
-           }else{
-               result[ind]=num1;
-               if(lo<=nums.length-1)
-                    lo++;
-               num1=nums[lo]*nums[lo];
-           }
-           ind--;
+        //    }else{
+        //        result[ind]=num1;
+        //        if(lo<=nums.length-1)
+        //             lo++;
+        //        num1=nums[lo]*nums[lo];
+        //    }
+        //    ind--;
            
-        }
-        return result;
+        // }
+        // return result;
     }
 }
