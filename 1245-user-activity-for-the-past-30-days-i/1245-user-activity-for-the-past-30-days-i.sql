27a6e3c82084e715e7b92851e36c4fc191a1a403
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-07-02 17:02:57 +0530

    Time: 893 ms (79.11%), Space: 0B (100%) - LeetHub

--- 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
@@ -1,2 +1,3 @@
-select activity_date as day , count(distinct user_id ) as active_users from  Activity
-where activity_date >= '2019-06-28' and activity_date <= '2019-07-27' group by activity_date;
+select activity_date as day , count(distinct user_id) as active_users from Activity 
+where activity_date >='2019-06-28' and activity_date <= '2019-07-27'
+group by activity_date; 
