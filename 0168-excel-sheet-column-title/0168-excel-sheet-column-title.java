756a4452899b970562125f6a51c5d77705cbddaa
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-08-22 21:38:58 +0530

    Time: 6 ms (34.08%), Space: 40.1 MB (41.23%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -3,26 +3,16 @@ class Solution {
         String[] str = new String[]{"Z",
     "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
     "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
-    "U", "V", "W", "X", "Y", "Z"
+    "U", "V", "W", "X", "Y"
 };
-        // columnNumber=25;
 
         String ans="";
         int flag=0;
         while(columnNumber!=0){
             int ind=columnNumber%26;
-            // if(ind==0){
-            //     columnNumber=columnNumber/26;
-            //     columnNumber-=1;
-            //     ans="Z"+ans;
-
-            // }
-            // else{
-                ans=str[ind]+ans;
-                if(ind==0){columnNumber-=1;}
-                columnNumber=columnNumber/26;
-
-            // }
+            ans=str[ind]+ans;
+            if(ind==0){columnNumber-=1;}
+            columnNumber=columnNumber/26;
             System.out.println("ind :"+ind);
             System.out.println("columnNumber :"+columnNumber);
 
