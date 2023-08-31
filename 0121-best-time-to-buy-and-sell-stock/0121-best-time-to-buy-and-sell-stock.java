5fd951db61df6c287b1380d64ec83438c8c2fe4e
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-08-31 23:54:39 +0530

    Time: 1 ms (99.93%), Space: 60.7 MB (84.52%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -33,16 +33,27 @@ class Solution {
         // }
         // return max;
 //------------further optmizattion solution in O(n) with constant space--------//
-        int maxProfit=Integer.MIN_VALUE;
-        int min=Integer.MAX_VALUE;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]<min){
-                min=nums[i];;
-            }
-            if(nums[i]-min>maxProfit){
-                maxProfit=nums[i]-min;
-            }
+        if(nums.length==1) return 0;
+
+        int min=nums[0];
+        int max_profit=Integer.MIN_VALUE;
+        int flag=1;
+
+        for(int i=1;i<nums.length;i++){
+               
+                if(nums[i]<min){ 
+                        min=nums[i];
+                }else{
+                        flag=0;
+                        int temp=nums[i]-min;
+                        if(temp>max_profit) max_profit=temp;
+                }
+                
         }
-        return maxProfit;
+        if(flag==1) return 0;
+
+        return max_profit;
+
+
     }
 }
