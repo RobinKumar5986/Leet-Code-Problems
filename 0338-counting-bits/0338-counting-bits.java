227570e86fe24d2a7ef8f1ac57185cf3e959278c
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-09-02 00:24:04 +0530

    Time: 2 ms (73.73%), Space: 46.4 MB (51.44%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,17 +1,22 @@
 class Solution {
-    int binCount(int n){
-        int count=0;
-        while(n!=0){
-            int rem=n%2;
-            if(rem==1) count++;
-            n=n>>1;
-        }
-        return count;
-    }
+    // int binCount(int n){
+    //     int count=0;
+    //     while(n!=0){
+    //         int rem=n%2;
+    //         if(rem==1) count++;
+    //         n=n/2;
+    //     }
+    //     return count;
+    // }
     public int[] countBits(int n) {
         int ans[]=new int[n+1];
-        for(int i=0;i<=n;i++){
-            ans[i]=binCount(i);
+        ans[0]=0;
+        for(int i=1;i<=n;i++){
+            if(i%2==1){
+                ans[i]=ans[i>>1]+1;
+            }else{
+                ans[i]=ans[i>>1];
+            }
         }
         return ans;
     }
