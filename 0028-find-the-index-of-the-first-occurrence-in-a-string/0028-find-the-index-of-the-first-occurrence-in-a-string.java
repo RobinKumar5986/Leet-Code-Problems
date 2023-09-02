2793f6a1658404b6168304387a0f5ba8e30d3933
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-09-03 01:46:29 +0530

    Time: 1 ms (33.05%), Space: 40.6 MB (31.02%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,6 +1,42 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        if(haystack.startsWith(needle))return 0;
-        return haystack.indexOf(needle);
+        if(haystack.length()< needle.length()) return -1;
+    //-----------Brute Force Solution-------------//
+        // int ans=-1;
+        // for(int i=0;i<haystack.length();i++){
+        //     if(haystack.charAt(i)==needle.charAt(0)){
+        //         ans=i;
+        //         int temp=i;
+        //         for(int j=0;j<needle.length();j++){
+        //             if(temp<haystack.length()){
+        //                 if(needle.charAt(j)==haystack.charAt(temp)){
+        //                     temp++;
+        //                 }else{
+        //                     ans=-1;
+        //                 }
+        //             }else{
+        //                 ans=-1;
+        //             }
+        //         }
+        //         if(ans!=-1) return ans;
+        //     }
+
+        // }
+        // return ans;
+    //-----------Bit Optmize Solution--------------//
+        
+        for(int i=0;i<=haystack.length()-needle.length();i++){
+            if(haystack.charAt(i)==needle.charAt(0)){
+                boolean found=true;
+                for(int j=0;j<needle.length();j++){
+                    if(haystack.charAt(j+i)!=needle.charAt(j)){ 
+                        found=false; break;
+                    }
+                }
+                if(found) return i;
+            }
+            
+        }
+        return -1;
     }
 }
