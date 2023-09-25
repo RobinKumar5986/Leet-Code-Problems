17b8ef56b0a9d6c8b940205d310dc25edc0daf8d
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-09-25 23:17:46 +0530

    Time: 2 ms (68.98%), Space: 40.2 MB (98.83%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,21 +1,10 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        int freq1[]=new int[256];
+        int c=(int) t.charAt( t.length()-1 );
         for(int i=0;i<s.length();i++){
-            freq1[s.charAt(i) - 'a']+=1;
+            c=c^s.charAt(i);
+            c=c^t.charAt(i);
         }
-
-        for(int i=0;i<t.length();i++){
-            freq1[t.charAt(i) - 'a']-=1;
-        }
-
-        for(int i=0;i<256;i++){
-            if(freq1[i]!=0)
-            {
-                int x=i+97;  //ascii value of 'a';
-                return (char)x ;
-            }
-        }
-        return 'a'; //just a dummy value
+        return (char)c;
     }
 }
