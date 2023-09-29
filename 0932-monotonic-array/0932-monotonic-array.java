58eb7b98f168571402b32236cd03614025ddb51c
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-09-30 01:41:56 +0530

    Time: 2 ms (62.8%), Space: 58.4 MB (12.05%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.java
+++ 0932-monotonic-array/0932-monotonic-array.java
@@ -5,8 +5,10 @@ class Solution {
         for(int i=1;i<nums.length;i++){
             if(nums[i] < nums[i-1]) inc=false;
             if(nums[i] > nums[i-1]) dec=false;
+
+            if(!(inc || dec)) return false;
         }
+        return true;
         
-        return inc || dec;    
     } 
 }
