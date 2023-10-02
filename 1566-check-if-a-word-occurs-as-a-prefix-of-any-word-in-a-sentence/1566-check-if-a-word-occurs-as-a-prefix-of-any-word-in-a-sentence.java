0ebae6b8eff7a5e45de90125db3d6ee285ed5705
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-10-03 00:58:33 +0530

    Time: 0 ms (100%), Space: 40.1 MB (72.05%) - LeetHub

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
@@ -1,36 +1,31 @@
 class Solution {
+    public boolean isPrefix(String sentence,String searchWord,int i){
+        boolean flag=true;
+        for(int j=0;j<searchWord.length() ;j++){
+            if(sentence.charAt(i+j)!=searchWord.charAt(j))
+            {
+                flag=false;
+                break;
+            }
+        }
+        return flag;
+    }
+
+
     public int isPrefixOfWord(String sentence, String searchWord) {
         int count=1;
         for(int i=0;i<sentence.length() ;i++){
             if(sentence.charAt(i)==searchWord.charAt(0)){
                 if(i==0){
-                    boolean flag=true;
-                    for(int j=0;j<searchWord.length() ;j++){
-                        if(sentence.charAt(i+j)!=searchWord.charAt(j))
-                        {
-                            flag=false;
-                            break;
-                        }
-                    }
-                    if(flag==true) return count;
+                    boolean flag=isPrefix(sentence,searchWord,i);
+                    if(flag) return count;
                 }
                 else{
                     if(sentence.charAt(i-1)==' '){
-                        boolean flag=true;
-                        for(int j=0;j<searchWord.length() ;j++){
-                            if(sentence.charAt(i+j)!=searchWord.charAt(j))
-                            {
-                                flag=false;
-                                break;
-                            }
-                        }
-
-                        if(flag==true) return count;
-
+                        boolean flag=isPrefix(sentence,searchWord,i);
+                        if(flag) return count;
                     }
-
                 }
-                
             }
             if(sentence.charAt(i)==' ') count++;
         }
