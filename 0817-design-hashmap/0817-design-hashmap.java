2241c5a08e0112e565f06d17bb53d3d90c3f3a31
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-10-04 19:31:43 +0530

    Time: 32 ms (24.67%), Space: 54.1 MB (32.7%) - LeetHub

---
+++ 0817-design-hashmap/0817-design-hashmap.java
@@ -0,0 +1,27 @@
+class MyHashMap {
+    int data[];
+    public MyHashMap() {
+        this.data=new int[1000001];
+        Arrays.fill(data,-1);
+    }
+    
+    public void put(int key, int value) {
+       this.data[key]=value;
+    }
+    
+    public int get(int key) {
+        return this.data[key];
+    }
+    
+    public void remove(int key) {
+        this.data[key]=-1;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
