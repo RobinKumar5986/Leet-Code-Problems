e5d081867be8b9e97b5ba86958315cc5a8bffd68
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-10-08 01:48:01 +0530

    Time: 16 ms (7.61%), Space: 45.7 MB (98.68%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -3,14 +3,13 @@ class Solution {
         int n_by_3=nums.length/3;
         HashMap<Integer,Integer> map=new HashMap<>();
         List<Integer> ans=new ArrayList<>();
+
         for(int i=0;i<nums.length;i++){
             if(!map.containsKey(nums[i]) ){
                 map.put(nums[i],1);
-
                 if( map.get(nums[i]) > n_by_3 && !ans.contains(nums[i] )) ans.add(nums[i]);
             }else{
                 map.put(nums[i] , map.get(nums[i]) + 1);
-
                 if( map.get(nums[i]) > n_by_3 && !ans.contains(nums[i] )) ans.add(nums[i]);
             }
         }
