80cb53a7ca38b33e9079c215e45a7891e0aa07fd
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-10-09 15:52:17 +0530

    Time: 0 ms (100%), Space: 44.3 MB (54.18%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,16 +1,14 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        if(nums.length<=0) return  new int[]{-1,-1};
-        if(nums.length==1 && nums[0]==target){
-            return new int[]{0,0};
-        }else if(nums.length==1 && nums[0]!=target){
-            return  new int[]{-1,-1};
-        }
+        if(nums.length<=0) return  new int[]{-1,-1};    //edge case
+        if(nums.length==1 && nums[0]==target) return new int[]{0,0};    //edge case
+        else if(nums.length==1 && nums[0]!=target) return  new int[]{-1,-1};  //edge case
+
         for(int i=0;i<nums.length;i++){
             if(nums[i]==target){
                 if(i+1<nums.length){
-                    
-                    if(nums[nums.length-1]==target)  return new int[]{i,nums.length-1};
+
+                    if(nums[nums.length-1]==target)  return new int[]{i,nums.length-1}; //edge case 
 
                     for(int j=i+1;j<nums.length;j++){
                         if(nums[j]!=target){
