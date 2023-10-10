3146986c329a9dbac4c7dffcb8a1ab3ef87df5be
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-10-10 23:32:03 +0530

    Time: 1 ms (97.44%), Space: 39.5 MB (52.99%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x==0) return 0;
+    
+        long l=0;
+        long r=x;
+        int ans=0;
+        while(l<=r){
+            long mid=(l+r)/2;
+            long sqr=mid*mid;
+            if(sqr==x){
+                return (int)mid;
+            }else if(sqr<x){
+                l=mid+1;
+            }else{
+                r=mid-1;
+            }
+            if(sqr<x)
+                ans=(int)mid;
+        }
+        return ans;
+
+    }
+}
