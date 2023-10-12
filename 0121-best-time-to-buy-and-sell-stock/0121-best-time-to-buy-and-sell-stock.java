4e65052335937d67f519f463cad700f08db8a948
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-10-12 23:04:59 +0530

    Time: 1 ms (99.95%), Space: 60.8 MB (70.76%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,5 +1,38 @@
 class Solution {
     public int maxProfit(int[] nums) {
+        //------------------Brute Foce--------------------//
+        // int max=Integer.MIN_VALUE;
+        // for(int i=0;i<nums.length-1;i++){
+
+        //     for(int j=i+1;j<nums.length;j++){
+        //         if(nums[i]<nums[j] && max<(nums[j]-nums[i])){
+        //             max=nums[j]-nums[i];
+        //         }
+        //     }
+        // }
+        // if(max==Integer.MIN_VALUE){
+        //     return 0;
+        // }
+        // return max;
+
+//----------------Otmized Solution in O(n) time (using space)--------------//
+        // int aux[]=new int[nums.length];
+        // int n=nums.length-1;
+        // aux[n]=nums[n];
+        // for(int i=n-1;i>=0;i--){
+        //     if(nums[i]>aux[i+1]){
+        //         aux[i]=nums[i];
+        //     }else{
+        //         aux[i]=aux[i+1];
+        //     }
+        // }
+        // int max=0;
+        // for(int i=0;i<nums.length;i++){
+        //     int temp=aux[i]-nums[i];
+        //     if(temp>max) max=temp;
+        // }
+        // return max;
+//------------further optmizattion solution in O(n) with constant space--------//
         if(nums.length==1) return 0;
         int min=nums[0];
         int max_profit=Integer.MIN_VALUE;
