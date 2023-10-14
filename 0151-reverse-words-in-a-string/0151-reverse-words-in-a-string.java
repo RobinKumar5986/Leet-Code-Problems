cb81e902781434a2aacee2e6be92232a377b11f4
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-10-15 00:14:43 +0530

    Time: 12 ms (32.57%), Space: 44.2 MB (27.18%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,27 +1,33 @@
 class Solution {
     
     public String reverseWords(String s) {
+        // String ans="";
+        // String temp="";
+        // for(int i=s.length()-1 ;i>=0 ;i--){
+        //     if(s.charAt(i)!=' '){
+        //         temp=s.charAt(i)+temp;
+        //     }else{
+        //         if(temp!=""){
+        //             if(ans=="")
+        //                 ans=ans+temp;
+        //             else
+        //                 ans=ans+" "+temp;
+        //             temp="";
+        //         }
+        //     }
+        // }
+        // if(temp!="") {
+        //    if(ans=="")
+        //         ans=ans+temp;
+        //     else
+        //         ans=ans+" "+temp;
+        //     temp="";
+        // }
         String ans="";
-        String temp="";
-        for(int i=s.length()-1 ;i>=0 ;i--){
-            if(s.charAt(i)!=' '){
-                temp=s.charAt(i)+temp;
-            }else{
-                if(temp!=""){
-                    if(ans=="")
-                        ans=ans+temp;
-                    else
-                        ans=ans+" "+temp;
-                    temp="";
-                }
-            }
-        }
-        if(temp!="") {
-           if(ans=="")
-                ans=ans+temp;
-            else
-                ans=ans+" "+temp;
-            temp="";
+        String[] words=s.split("\\s+");
+        for(String w : words){
+            if(ans=="") ans=w;
+            else ans=w+" "+ans;
         }
         return ans;
     }
