a11d2d69323914c772d6d13876906bc1ad0de67b
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-10-15 19:07:11 +0530

    Time: 1399 ms (75.86%), Space: 0B (100%) - LeetHub

---
+++ 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,10 @@
+with cte as(select product_id, sum(unit) as unit
+from Orders
+where YEAR(order_date)=2020
+and MONTH(order_date)=2
+group by product_id)
+
+select product_name, unit from Products ,cte
+where Products.product_id=cte.product_id
+and unit>=100;
+
