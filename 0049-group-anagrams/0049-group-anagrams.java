c9700cee88a74083983e34da73773ab974b53710
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-10-15 19:55:04 +0530

    Time: 5 ms (98.87%), Space: 47.3 MB (19.47%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,49 +1,28 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-//------------Solution---------------------//
-        // HashMap<HashMap<Character,Integer> ,ArrayList<String>> bmap=new HashMap<>();
-        // for(String str : strs ){
-        //     HashMap<Character,Integer> fmap=new HashMap<>();
-        
-        //     for(int i=0;i<str.length();i++){
-        //         char c=str.charAt(i);
-        //         fmap.put(c,fmap.getOrDefault(c,0) + 1);
-        //     }
+        HashMap<String,List<String>> map=new HashMap<>();
 
-        //     if(! bmap.containsKey(fmap)){
-        //         ArrayList<String> list=new ArrayList<>();
-        //         list.add(str);
-        //         bmap.put(fmap,list);
-        //     }else{
-        //         ArrayList<String> list=bmap.get(fmap);
-        //         list.add(str);
-        //     }
-        // }
-        // List<List<String>> result=new ArrayList<>();
-        // for(ArrayList<String> val : bmap.values()){
-        //     result.add(val);
-        // }
-        // return result;
-//-------------more optmizes solution-----------//
-    HashMap<String,List<String>> map=new HashMap<>();    
-        for(int i=0;i<strs.length;i++){
-            String s1=strs[i];
-            char[] arr=s1.toCharArray();
+        for(int i=0;i<strs.length; i++){
+            String s=strs[i];
 
-            Arrays.sort(arr);
-            String str=new String(arr);
+            char[] letters=s.toCharArray(); //converting the array 
+            Arrays.sort(letters);           //sorting the array
+            s=new String(letters);          //converting back to String
 
-            if(map.containsKey(str)){
-
-                map.get(str).add(s1);
-                
-            }else{
-                map.put(str,new ArrayList<>());
-                map.get(str).add(s1);
+            if(!map.containsKey(s)){
+                List<String> group=new ArrayList<>();
+                group.add(strs[i]);
+                map.put(s,group);
             }
+            else{
+                List<String> group=map.get(s);
+                group.add(strs[i]);
+                map.put(s,group);
+            }
+
+
         }
+        
         return new ArrayList<>(map.values());
-
-
     }
 }
