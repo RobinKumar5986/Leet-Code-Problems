54e8ffd522ce112d10fbe1a35183b2ce41afa0e4
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-12-16 05:11:02 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (40.14%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,84 +10,36 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1==null) return list2;
+        if(list1==null) return list2; //edge case
         if(list2==null) return list1;
 
-        ListNode node=null;
-        ListNode prev=null;
+        ListNode node=new ListNode();
         ListNode head=null;
+        boolean flag=true;
+    
         while(list1!=null && list2!=null){
-            int v1=list1.val;
-            int v2=list2.val;
-            ListNode s1=list1.next;
-            ListNode s2=list2.next;
-            if(node==null){
-                if(v1<=v2){
-                    head=list1;
-                    node=list1;
+           if(list1.val<=list2.val){
+               if(flag)
+                  head=list1;
+                flag=false;
 
-                    list1=s1;
-                }else{
-                    head=list2;
-                    node=list2;
+               node.next=list1;
+               list1=list1.next;
+           }else{
+               if(flag)
+                  head=list2;
+                flag=false;
 
-                    list2=s2;
-                }
-            }else{
-                int v3=node.val;
-                if(v1<=v2){
-                    if(v1<=v3){
-                        node.val=v1;
-                        node.next=list1;
-                        node=list1;
-                        list1=s1;
-                    }else{
-                        node.next=list1;
-                        node=list1;
-                        list1=s1;
-                    }
-                }else{
-                    if(v2<=v3){
-                        node.val=v2;
-                        node.next=list2;
-                        node=list2;
-
-                        list2=s2;
-                    }else{
-                        node.next=list2;
-                        node=list2;
-
-                        list2=s2;
-
-                    }
-                }
-            }
-     
+               node.next=list2;
+               list2=list2.next;
+           }
+           node=node.next;
         }
-        while(list1!=null ){
-            int v1=list1.val;
-            int v2=node.val;
-            if(v1<=v2)
-                node.val=v1;
-            else
-                node.val=v2;
-            
+        if(list1!=null){
             node.next=list1;
-            node=list1;
-            list1=list1.next;  
         }
-        while(list2!=null){
-            int v1=list2.val;
-            int v2=node.val;
-            if(v1<=v2)
-                node.val=v1;
-            else
-                node.val=v2;
-            
+        if(list2!=null){
             node.next=list2;
-            node=list2;
-            list2=list2.next;
-
         }
     
         return head;
