13fc4ce9fc80b5fec7aa3d639b4f4a73c281922c
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-12-16 12:15:08 +0530

    Time: 5 ms (54.58%), Space: 42.3 MB (74.32%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,17 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-//---------Brute Force Using Mapping---------//
-        if(s.length()!=t.length() || s.length()<1) return false;
-        int[] flag=new int[256];
+        if(s.length() != t.length() ) return false;
 
-        for(int i=0; i<s.length() ;i++){
-            flag[s.charAt(i)]+=1;
-            flag[t.charAt(i)]-=1;
+        int[] m1=new int[26];
+        int[] m2=new int[26];
+        for(int i=0;i<s.length() ; i++){
+            m1[s.charAt(i) - 'a']++;
+            m2[t.charAt(i) - 'a']++;
         }
-
-        for(int i=0;i<256 ;i++){
-            if(flag[i]!=0) return false;
-        }
-        return true;
+        if( Arrays.equals(m1,m2) ) return true;
+        return false;
     }
 }
