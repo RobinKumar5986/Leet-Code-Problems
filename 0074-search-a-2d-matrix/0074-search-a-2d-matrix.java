3531ce12486942074c872bf4b05dec5150ca5a64
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-12-18 02:42:05 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (44.21%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,21 +1,27 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        //finding the row
-        int col=matrix[0].length-1;
+        //finding the row using binary search
         int row=0;
-        for(int i=0;i<matrix.length;i++){
-            if(matrix[i][col] == target) return true;
-
-            if(matrix[i][col] > target){
-                // System.out.println(matrix[i][col]);
-                row=i;
+        int top=0;
+        int bot=matrix.length-1;
+        while(top<=bot){
+            int mid=(top+bot)/2;
+            if(target < matrix[mid][0]){
+                bot=mid-1;
+            }else if(target > matrix[mid][matrix[0].length-1]){
+                top =mid+1;
+            }else if(target >= matrix[mid][0] && target <= matrix[mid][matrix[0].length-1] ){
+                row=mid;
                 break;
             }
         }
-        System.out.println(row);
-        for(int i=0;i<=col ; i++){
-            if(matrix[row][i]==target)
-                return true;
+        int lo=0;
+        int hi=matrix[0].length-1;
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            if(matrix[row][mid]==target) return true;
+            else if(matrix[row][mid] < target) lo=mid+1;
+            else hi=mid-1;
         }
         return false;
     }
