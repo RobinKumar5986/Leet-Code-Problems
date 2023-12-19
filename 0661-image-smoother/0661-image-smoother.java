fb4d2be90dd9e06e5c9481fe43469c9c6a55c537
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2023-12-20 03:31:13 +0530

    Time: 5 ms (89.72%), Space: 45.4 MB (8.61%) - LeetHub

---
+++ 0661-image-smoother/0661-image-smoother.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[][] imageSmoother(int[][] img) {
+        
+        int m=img.length;
+        int n=img[0].length;
+        int[][] ans=new int[m][n];
+
+        for(int i=0;i < m ;i++){
+            for(int j=0; j < n ; j++){
+                int count=0;
+                int sum=0;
+                for(int x=i-1 ; x <= i+1; x++){
+                    for(int y=j-1 ; y <=j+1;y++){
+
+                        if( x >=0 && x < m && y>=0 && y<n){
+                            count++;
+                            sum +=img[x][y];
+                        }
+                    }
+                }
+    
+                ans[i][j]=sum/count;
+            }
+        }
+
+        return ans;
+    }
+}
