97e71e7464203557a0b0f6702f9d3473b225a545
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-01-13 03:37:14 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (51.20%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    int findLen(ListNode head,int len){
+        while(head!=null){
+            len++;
+            head=head.next;
+        }
+        return len;
+    }
+    ListNode removeBack(ListNode head,int len){
+        if(len==1) return null;
+        else{
+            ListNode save=head;
+            ListNode prev=null;
+            ListNode itr=head;
+            while(itr.next!=null){
+                prev=itr;
+                itr=itr.next;
+            }
+            prev.next=null;
+            head=save;
+        }
+        return head;
+    }
+    ListNode removeInd(ListNode head,int len,int n){
+        ListNode save=head;
+        n=len-n;
+        int co=0;
+        ListNode prev=null;
+        ListNode next=head.next;
+        while(co!=n){
+            prev=head;
+            head=next;
+            next=next.next;
+            co++;
+        }
+        prev.next=next;
+        return save;
+    }
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        int len=0;
+        len=findLen(head,len);
+        if(n==1){
+            head=removeBack(head,len);
+        }else if(n==len){
+            head=head.next;
+        }
+        else{
+            head=removeInd(head,len,n);
+        }
+        return head;
+    }
+}
