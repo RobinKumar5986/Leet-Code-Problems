2f507af2d56d912ae2fa068e291ed5cba85e07fe
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-01-14 02:31:46 +0530

    Time: 8 ms (76.50%), Space: 45.4 MB (18.66%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -1,41 +1,17 @@
 class Solution {
-    Map<Character,Integer> mapper(Map<Character,Integer> map,String s){
-        for(int i=0;i<s.length();i++)
-        {
-            char c=s.charAt(i);
-            map.put(c,map.getOrDefault(c,0) +1 );
-        }
-        return map;
-    }
     public int minSteps(String s, String t) {
         if(s.length()!=t.length()) return -1;
-
-        Map<Character,Integer> map1=new HashMap<>();
-        Map<Character,Integer> map2=new HashMap<>();
-        map1=mapper(map1,s);
-        map2=mapper(map2,t);
-        int steps=0;
-        int unique=0;
-        for(Map.Entry<Character,Integer> ele : map1.entrySet()){
-            char key=ele.getKey();
-            int val=ele.getValue();
-            if(map2.containsKey(key)){
-                int val2=map2.get(key);
-                if(val<val2)
-                    steps+=val2-val;
-                else
-                    steps+=val-val2;
-            }else{
-                unique+=val;
-            }       
+        char[] map1=new char[26];
+        char[] map2=new char[26];
+        for(int i=0;i<s.length();i++){
+            map1[s.charAt(i) - 'a']++;
+            map2[t.charAt(i) - 'a']++;
         }
-        for(Map.Entry<Character,Integer> ele : map2.entrySet()){
-            char key=ele.getKey();
-            int val=ele.getValue();
-            if(!map1.containsKey(key))
-                unique+=val;
+        int step=0;
+        for(int i=0;i<26;i++){
+            if(map1[i]!=map2[i])
+                step+=Math.abs(map1[i]-map2[i]);
         }
-        steps+=unique;
-        return steps/2;
+        return step/2;
     }
 }
