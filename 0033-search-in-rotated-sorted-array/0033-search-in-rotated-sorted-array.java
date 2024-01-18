5bb0b7e78a39027be9a23a779a2f2c54fff02b45
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-01-19 02:47:09 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (13.27%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,49 +1,19 @@
 class Solution {
     public int search(int[] nums, int target) {
-//--------------in O(n) brute force---------------//
-        // for(int i=0;i<nums.length;i++){
-        //     if(nums[i]==target){
-        //         return i;
-        //     }
-        // }
-        // return -1
-//------------in O(n/2) in worst case-----------//
-        // int start=0;
-        // int last=nums.length-1;
-        // while(start<=last){
-        //     if(nums[start]==target){
-        //         return start;
-        //     }
-        //     if(nums[last]==target){
-        //         return last;
-        //     }
-        //     start++;
-        //     last--;
-        // }
-        // return -1;
+        int hi=nums.length-1;
+        int lo=0;
 
-//------optimal solution in O(log(n)) binary rearch---------//
-
-        int start=0;
-        int end=nums.length-1;
-        while(start<=end){
-            int mid=(start+end)>>1;
-            if(nums[mid]==target){
-                return mid;
-            }
-            if(nums[start]<=nums[mid]){
-                if(target>=nums[start] && target<=nums[mid]){
-                    end=mid-1;
-                }else{
-                    start=mid+1;
-                }
-            }else{
-                if(target>=nums[mid] && target<=nums[end]){
-                    start=mid+1;
-                }else{
-                    end=mid-1;
-                }
-            }
+        while(lo<=hi){
+            int mid=(hi+lo)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[lo]<=nums[mid] && target>=nums[lo] && target<nums[mid])
+                hi=mid-1;
+            else if(nums[mid]<=nums[hi] && target>nums[mid] && target<=nums[hi])
+                lo=mid+1;
+            else if(nums[lo]>nums[mid] && (target >=nums[lo] || target<nums[mid]))
+                hi=mid-1;
+            else
+                lo=mid+1;
         }
         return -1;
     }
