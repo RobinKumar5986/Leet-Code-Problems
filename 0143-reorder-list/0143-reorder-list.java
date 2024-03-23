50ab29455fdad249f2e6a3710fead13d4afbca5f
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-03-23 16:47:50 +0530

    Time: 2 ms (88.69%), Space: 47.9 MB (82.17%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -1,43 +1,31 @@
 class Solution {
-    void display(ListNode head){
-        while(head!=null){
-            System.out.print(head.val+ " ");
-            head=head.next;
-        }
-        System.out.println();
-    }
     public void reorderList(ListNode head) {
 
         ListNode fast=head;
         ListNode slow=head;
-        //Getting the Midlle of the List
-        while(fast!=null && fast.next!=null){
+        while(fast.next !=null && fast.next.next!=null){
             slow=slow.next;
             fast=fast.next.next;
         }
-        
-        //Reversing the List
-        ListNode second=slow.next;
-        ListNode prev=slow.next=null;
-
+        ListNode second= slow.next;
+        slow.next=null;
+        ListNode prev=null;
         while(second!=null){
             ListNode temp=second.next;
             second.next=prev;
             prev=second;
             second=temp;
         }
-        second=prev;
-        //Rearranging the List assignig
         ListNode first=head;
+        second=prev;
         while(second!=null){
             ListNode t1=first.next;
             ListNode t2=second.next;
-            //Creatig Connection
+
             first.next=second;
             second.next=t1;
-            //Mpving Ahade
             first=t1;
-            second=t2;   
+            second=t2;
         }
     }
 }
