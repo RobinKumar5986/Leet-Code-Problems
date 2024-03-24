58c1bb1753588df6613110e92355c9529d2ff488
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-03-24 19:13:07 +0530

    Time: 4 ms (85.61%), Space: 58.2 MB (45.98%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,29 +1,20 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-//----------Solution with Space Complexity----------//
-        // Set<Integer> map=new HashSet<>();
-        // for(int i=0;i<nums.length;i++){
-        //     if(map.contains(nums[i])){
-        //         return nums[i];
-        //     }else{
-        //         map.add(nums[i]);
-        //     }
-        // }
-        // return -1;
-
-//----------Solution using turtle method-------//
-        int slow=nums[0];
-        int fast=nums[0];
-        do{
-            slow=nums[slow];
+        int fast=0;
+        int slow=0;
+        while(true){
             fast=nums[nums[fast]];
-        }while(slow!=fast);
-        fast=nums[0];
-        while(slow!=fast){
             slow=nums[slow];
-            fast=nums[fast];
+            if(fast==slow)
+                break;
         }
-
+        int slow2=0;
+        while(true){
+            slow=nums[slow];
+            slow2=nums[slow2];
+            if(slow==slow2)
+                break;
+        }
         return slow;
     }
 }
