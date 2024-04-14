bad0c5f0864dd66feb96ff868fdbf156ad140d4f
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-04-15 00:57:21 +0530

    Time: 13 ms (76.89%), Space: 45.9 MB (22.19%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,27 +1,27 @@
 class Solution {
+    void appendChar(StringBuilder sb,char c, int freq){
+        for(int i=0;i<freq;i++)
+            sb.append(c);
+    }
     public String frequencySort(String s) {
-        int[] freq=new int[256];
-        for(int i=0;i<s.length();i++)
-            freq[s.charAt(i)]++;
-        String ans="";
-        for(int i=0;i<256;i++){
-            int max=Integer.MIN_VALUE;
-            int ind=-1;
-            for(int j=0;j<256;j++){
-                if(freq[j]>max){
-                    max=freq[j];
-                    ind=j;
-                }
-            }
-            if(max>0){
-                char c=(char)ind;
-                ans+=String.valueOf(c).repeat(max);
-                freq[ind]=Integer.MIN_VALUE;
-            }
-            else{
-                return ans;
+        Map<Character,Integer> map=new HashMap<>();
+        List<Character> bucket[]=new ArrayList[s.length()+1];
+        StringBuilder sb=new StringBuilder();
+        for(char c : s.toCharArray())
+            map.put(c,map.getOrDefault(c,0)+1);
+        for(Map.Entry<Character,Integer> ele : map.entrySet()){
+            char val=ele.getKey();
+            int  ind=ele.getValue();
+            if(bucket[ind]==null)
+                bucket[ind]=new ArrayList<>();
+            bucket[ind].add(val);  
+        }
+        for(int i=bucket.length-1;i>=0;i--){
+            if(bucket[i]!=null){
+                for(char ele : bucket[i] )
+                    appendChar(sb,ele,i);
             }
         }
-        return ans;
+        return new String(sb);
     }
 }
