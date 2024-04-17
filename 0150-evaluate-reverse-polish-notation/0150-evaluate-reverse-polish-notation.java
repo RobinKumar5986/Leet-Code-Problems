5a1f8707cf44327d469d85b9b69874c35064d761
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-04-17 16:53:11 +0530

    Time: 6 ms (75.02%), Space: 44.4 MB (74.57%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -2,21 +2,29 @@ class Solution {
     public int evalRPN(String[] nums) {
         Stack<Integer> st=new Stack<>();
         for(int i=0;i<nums.length;i++){
-            String ch=nums[i];
-
-            if(!ch.equals("+") && !ch.equals("-") && !ch.equals("*") && !ch.equals("/")){
-                int n=Integer.parseInt(ch);
-                st.push(n);
+            char c=nums[i].charAt(nums[i].length()-1);
+            if(c!='+' && c!='-' && c!='*' && c!='/'){
+                st.push(Integer.parseInt(nums[i]));
             }else{
-                int n2=st.pop();
                 int n1=st.pop();
-                if( ch.equals("+") ) st.push(n1+n2);
-                if( ch.equals("-") ) st.push(n1-n2);
-                if( ch.equals("*") ) st.push(n1*n2);
-                if( ch.equals("/") ) st.push(n1/n2);                 
+                int n2=st.pop();
+                switch(c){
+                    case '+':
+                        st.push(n2+n1);
+                        break;
+                    case '-':
+                        st.push(n2-n1);
+                        break;
+                    case '*':
+                        st.push(n2*n1);
+                        break;
+                    case '/':
+                        st.push(n2/n1);
+                        break;
+                }
             }
-        }
-        return st.peek();
 
+        }
+        return st.pop();   
     }
 }
