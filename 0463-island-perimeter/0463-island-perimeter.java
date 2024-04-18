717ddf5aa74934f5c90d5c92e277e04a5d2fabb4
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-04-18 13:02:44 +0530

    Time: 4 ms (99.58%), Space: 45.4 MB (81.43%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -2,30 +2,22 @@ class Solution {
     public int islandPerimeter(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
-        int para=0;
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1){
-                    if(i-1<0)
-                        para++;
-                    if(i+1>=m)
-                        para++;
-                    if(j-1<0)
-                        para++;
-                    if(j+1>=n)
-                        para++;
-                    if(i-1 >=0 && grid[i-1][j]==0)
-                        para++;
-                    if(i+1 <m && grid[i+1][j]==0)
-                        para++;
-                    if(j-1>=0 && grid[i][j-1]==0)
-                        para++;
-                    if(j+1 <n && grid[i][j+1]==0)
-                        para++;
-                }
+        int ans=0;
+        for(int row=0;row<m;row++){
+            for(int col=0;col<n;col++){
+                if(grid[row][col]!=1)
+                    continue;
+                if(row-1 < 0 || grid[row-1][col]==0)
+                    ans++;
+                if(row+1 >= m || grid[row+1][col]==0)
+                    ans++;
+                if(col-1 < 0 || grid[row][col-1]==0)
+                    ans++;
+                if(col+1 >= n || grid[row][col+1]==0)
+                    ans++;
             }
         }
-        return para;
+        return ans;
     }
 }
