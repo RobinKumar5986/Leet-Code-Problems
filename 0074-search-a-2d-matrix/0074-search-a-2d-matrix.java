b508c5431e97966745a2944ecee51e5e4747b603
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-05-04 13:55:31 +0530

    Time: 0 ms (100.00%), Space: 42 MB (59.01%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,27 +1,29 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        //finding the row using binary search
-        int row=0;
-        int top=0;
-        int bot=matrix.length-1;
-        while(top<=bot){
-            int mid=(top+bot)/2;
-            if(target < matrix[mid][0]){
-                bot=mid-1;
-            }else if(target > matrix[mid][matrix[0].length-1]){
-                top =mid+1;
-            }else if(target >= matrix[mid][0] && target <= matrix[mid][matrix[0].length-1] ){
-                row=mid;
-                break;
-            }
-        }
-        int lo=0;
-        int hi=matrix[0].length-1;
+    boolean secBin(int[] row,int lo,int hi,int t){
         while(lo<=hi){
             int mid=(lo+hi)/2;
-            if(matrix[row][mid]==target) return true;
-            else if(matrix[row][mid] < target) lo=mid+1;
-            else hi=mid-1;
+            if(row[mid]==t)
+                return true;
+            else if(row[mid]>t)
+                hi=mid-1;
+            else
+                lo=mid+1;
+        }
+        return false;
+    }
+    public boolean searchMatrix(int[][] mat, int t) {
+        int lo=0;
+        int hi=mat.length-1;
+        int len=mat[0].length-1;
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            if(t>=mat[mid][0] && t<=mat[mid][len]){
+                return secBin(mat[mid],0,len,t);
+            }else if(mat[mid][0]>t){
+                hi=mid-1;
+            }else{
+                lo=mid+1;
+            }
         }
         return false;
     }
