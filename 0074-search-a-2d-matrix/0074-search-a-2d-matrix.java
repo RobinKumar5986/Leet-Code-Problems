81fac8e1e2424612f018ca7886c23cef70fe905d
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-05-11 02:17:19 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (91.85%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,29 +1,18 @@
 class Solution {
-    boolean secBin(int[] row,int lo,int hi,int t){
-        while(lo<=hi){
-            int mid=(lo+hi)/2;
-            if(row[mid]==t)
-                return true;
-            else if(row[mid]>t)
-                hi=mid-1;
-            else
-                lo=mid+1;
-        }
-        return false;
-    }
-    public boolean searchMatrix(int[][] mat, int t) {
+    public boolean searchMatrix(int[][] mat, int target) {
         int lo=0;
-        int hi=mat.length-1;
-        int len=mat[0].length-1;
+        int row=mat.length;
+        int col=mat[0].length;
+
+        int hi=row*col-1;
         while(lo<=hi){
             int mid=(lo+hi)/2;
-            if(t>=mat[mid][0] && t<=mat[mid][len]){
-                return secBin(mat[mid],0,len,t);
-            }else if(mat[mid][0]>t){
-                hi=mid-1;
-            }else{
+            if(mat[mid/col][mid%col]==target)
+                return true;
+            else if(mat[mid/col][mid%col]<target)
                 lo=mid+1;
-            }
+            else 
+                hi=mid-1;
         }
         return false;
     }
