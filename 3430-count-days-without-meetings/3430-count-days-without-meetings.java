485f5e50b0256e876640c73129f1163852d6959f
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-06-03 00:18:10 +0530

    Time: 38 ms (100%), Space: 100.6 MB (100%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.java
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.java
@@ -5,7 +5,6 @@ class Solution {
         int prev=1;
         boolean flag=true;
         for(int[] ele : meet){
-            System.out.println(ele[0] + " : " +ele[1] );
             if(flag){
                 if(prev<ele[0])
                     ans+=ele[0]-prev;
@@ -18,7 +17,6 @@ class Solution {
             if(ele[1]>prev)
                 prev=ele[1];
         }
-        System.out.println(prev);
         if(days>prev)
             ans+=days-prev;
         return ans;
