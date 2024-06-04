99b7c6d44a28c62db848c18a08007541eb65be87
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-06-04 15:04:44 +0530

    Time: 3 ms (74.7%), Space: 41.4 MB (90.37%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] map=new int[256];
+        for(int i=0;i<s.length();i++)
+            map[s.charAt(i)]++;
+        boolean flag=true;
+        int ans=0;
+        for(int ele : map){
+            if(flag && ele%2!=0){
+                ans+=ele;
+                flag=false;
+            }
+            else if(ele>=2){
+                ele=ele/2;
+                ans+=ele*2;
+            }
+        }
+        return ans;
+    }
+}
