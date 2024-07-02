90227ecd981532003fb63b3097f3dc1371f12259
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-07-03 01:37:52 +0530

    Time: 1 ms (97.67%), Space: 43.2 MB (38.89%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -2,12 +2,17 @@ class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         int map1[] =new int[1000+1];
         int map2[] =new int[1000+1];
+        int max=0;
         List<Integer> tempAns=new ArrayList<>();
-        for(int ele : nums1)
+        for(int ele : nums1){
             map1[ele]++;
-        for(int ele : nums2)
+            max=Math.max(ele,max);
+        }
+        for(int ele : nums2){
             map2[ele]++;
-        for(int i=0;i<=1000;i++){
+            max=Math.max(ele,max);
+        }
+        for(int i=0;i<=max;i++){
             if(map1[i]==0 || map2[i]==0)
                 continue;
             int min=Math.min(map1[i],map2[i]);
