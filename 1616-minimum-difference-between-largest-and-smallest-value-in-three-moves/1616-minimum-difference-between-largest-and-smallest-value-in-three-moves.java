6062d9219bb2acb4bf4f4d9a4ab62f63f53869a5
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-07-04 00:56:16 +0530

    Time: 15 ms (96.06%), Space: 57.2 MB (41.99%) - LeetHub

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.java
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.java
@@ -7,12 +7,12 @@ class Solution {
         int n2=nums[nums.length-3]-nums[1];
         int n3=nums[nums.length-2]-nums[2];
         int n4=nums[nums.length-1]-nums[3];
+        if(n3<n1 && n3<n2 && n3<n4)
+            return n3;
         if(n2<n1 && n2<n3 && n2<n4)
             return n2;
         if(n1<n2 && n1<n3 && n1<n4)
             return n1;
-        if(n3<n1 && n3<n2 && n3<n4)
-            return n3;
         return n4;
     }
 }
