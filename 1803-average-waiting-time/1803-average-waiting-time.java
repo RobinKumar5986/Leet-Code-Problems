6f84cf3f5e4cdcc49d011d9bdffeb20a0bf81c64
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-07-10 00:31:54 +0530

    Time: 3 ms (93.06%), Space: 72.6 MB (58.37%) - LeetHub

--- 1803-average-waiting-time/1803-average-waiting-time.java
+++ 1803-average-waiting-time/1803-average-waiting-time.java
@@ -3,9 +3,8 @@ class Solution {
         int ft=0;
         double avg=0;
         for(int i=0;i<cust.length;i++){
-            if(ft<cust[i][0]){
+            if(ft<cust[i][0])
                 ft=cust[i][0];
-            }
             ft+=cust[i][1];
             avg+=ft-cust[i][0];
         }
