d148726f6cf829331f75c5503e2971cb2760fdb7
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-08-06 12:04:35 +0530

    Time: 7 ms (54.99%) | Memory: 44.5 MB (11.68%) - LeetSync

--- 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.java
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.java
@@ -4,20 +4,13 @@ class Solution {
         for(int i=0;i<arr.length;i++){
             map.put(arr[i],map.getOrDefault(arr[i],0)+1);
         }
-        List<Integer> val = new ArrayList<>(map.values());
-        Collections.sort(val);
-        if(k-1 < 0 || k-1 >= val.size())
-            return "";
-        int freq = val.get(k-1);
-        if(freq > k )
-            return "";
-        for(String ele : arr){
-            if(map.get(ele) <= freq){
-                k--;
-                if(k == 0 && map.get(ele) == freq)
+        int count =0;
+        for(String ele: arr){
+            if(map.get(ele)==1){
+                count++;
+                if(count==k){
                     return ele;
-                else if(k==0 && map.get(ele) != freq)
-                    return "";
+                }
             }
         }
         return "";
