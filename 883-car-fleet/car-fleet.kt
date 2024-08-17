d725278c89bd4969b253b5387270f0f96accc0c3
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-08-17 18:53:52 +0530

    Time: 634 ms (89.47%) | Memory: 54.7 MB (61.65%) - LeetSync

---
+++ 883-car-fleet/car-fleet.kt
@@ -0,0 +1,27 @@
+class Solution {
+    fun carFleet(target: Int, position: IntArray, speed: IntArray): Int {
+        val n = speed.size
+        if(n<=0)
+            return 0
+        var ans = 1
+        val map = position.indices.associate{
+            position[it] to speed[it]
+        }
+        val pos = position.sortedArrayDescending()
+        var min = -1.0
+        for(ele in pos){
+            val speed = map[ele]?: continue 
+            val time = (target - ele) / speed.toDouble()
+            if(min == -1.0){
+                min = time
+                println(time)
+            }else{
+                if(time > min){
+                    min = time
+                    ans++
+                }
+            }
+        }
+        return ans
+    }
+}
