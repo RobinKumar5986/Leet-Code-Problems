8e22e2346fc3ed3fa61b00db427928062fba7b72
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-08-17 18:59:22 +0530

    Time: 285 ms (5.36%) | Memory: 39.2 MB (42.56%) - LeetSync

---
+++ 792-binary-search/binary-search.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun search(nums: IntArray, target: Int): Int {
+        var lo = 0
+        var hi = nums.size-1
+        while(lo<=hi){
+            val mid = lo + (hi-lo)/2
+            if(nums[mid]==target)
+                return mid
+            else if(nums[mid] > target){
+                hi = mid-1
+            }else{
+                lo = mid+1
+            }
+        }
+        return -1
+    }
+}
