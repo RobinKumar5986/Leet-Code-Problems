8b5051cec1f4bebbe866a03424378321a06cc7f6
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-08-22 15:28:12 +0530

    Time: 5 ms (33.33%) | Memory: 15.4 MB (55.56%) - LeetSync

--- 476-number-complement/number-complement.swift
+++ 476-number-complement/number-complement.swift
@@ -2,7 +2,7 @@ class Solution {
     func findComplement(_ num: Int) -> Int {
         let bitLength = Int.bitWidth - num.leadingZeroBitCount
         let bitMask = (1 << bitLength) - 1
-
-        return (~num) & bitMask
+        var ans = (~num) & bitMask
+        return ans
     }
 }
