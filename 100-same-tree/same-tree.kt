53ecd7b844921351604c55210e49b0fd53a92ff5
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-08-31 01:52:58 +0530

    Time: 147 ms (44.08%) | Memory: 33.5 MB (89.30%) - LeetSync

--- 100-same-tree/same-tree.kt
+++ 100-same-tree/same-tree.kt
@@ -12,7 +12,7 @@ class Solution {
     fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
         if(p == null && q == null)
             return true
-        if( (p == null && q!=null ) || (p != null && q==null ) )
+        if( (p == null || q==null ) )
             return false
         if(p!!.`val` != q!!.`val`)
             return false
