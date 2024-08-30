2b6db06ba729f9a238f041b1031ceee2062009e3
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-08-31 01:11:37 +0530

    Time: 205 ms (24.79%) | Memory: 37.4 MB (95.77%) - LeetSync

---
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.kt
@@ -0,0 +1,31 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    var ans = 0 
+    fun dia(root: TreeNode?): Int{
+        if(root == null){
+            return 0
+        }
+        val left = dia(root.left)
+        val right = dia(root.right)
+
+        if(left + right > ans)
+            ans = left+right
+        return Math.max(left,right) + 1
+
+    }
+    fun diameterOfBinaryTree(root: TreeNode?): Int {
+        if(root == null || (root.left == null && root.right == null))
+            return 0
+        dia(root)
+        return ans
+    }
+}
