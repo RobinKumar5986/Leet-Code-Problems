89edd7593acc5f0346803259fa5eb9e54de78e19
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-08-31 01:27:37 +0530

    Time: 183 ms (51.96%) | Memory: 36.4 MB (92.17%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.kt
@@ -0,0 +1,30 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    var ans = true
+    fun determin(root: TreeNode?): Int {
+        if(root == null)
+            return 0
+        val l = determin(root.left)
+        val r = determin(root.right)
+
+        if(Math.abs(l-r) > 1){
+            ans = false
+        }
+        return Math.max(l,r)+1
+    }
+    fun isBalanced(root: TreeNode?): Boolean {
+        if(root == null)
+            return true
+        determin(root)
+        return ans
+    }
+}
