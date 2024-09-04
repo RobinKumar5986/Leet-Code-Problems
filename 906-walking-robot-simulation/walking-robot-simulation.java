bfefe36229b9a1243e79090a97ceba58f07aa6e8
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-09-04 19:03:05 +0530

    Time: 36 ms (42.79%) | Memory: 49.1 MB (72.49%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.java
+++ 906-walking-robot-simulation/walking-robot-simulation.java
@@ -3,21 +3,21 @@ class Solution {
     // Direction mapping
     private int changeDir(int dir, int cmd) {
         switch (dir) {
-            case 1: 
-                if (cmd == -1) dir = 3; 
-                else dir = 4; 
+            case 1: // ↑
+                if (cmd == -1) dir = 3; // Left: ←
+                else dir = 4; // Right: →
                 break;
-            case 2: 
-                if (cmd == -1) dir = 4; 
-                else dir = 3; 
+            case 2: // ↓
+                if (cmd == -1) dir = 4; // Right: →
+                else dir = 3; // Left: ←
                 break;
-            case 3: 
-                if (cmd == -1) dir = 2; 
-                else dir = 1; 
+            case 3: // →
+                if (cmd == -1) dir = 2; // Down: ↓
+                else dir = 1; // Up: ↑
                 break;
-            case 4: 
-                if (cmd == -1) dir = 1; 
-                else dir = 2; 
+            case 4: // ←
+                if (cmd == -1) dir = 1; // Up: ↑
+                else dir = 2; // Down: ↓
                 break;
         }
         return dir;
@@ -27,29 +27,28 @@ class Solution {
         int max = 0;
         Set<String> obs = new HashSet<>();
         for (int[] obstacle : obstacles) {
-            
             obs.add(obstacle[0] + "," + obstacle[1]);
         }
 
-        int x = 0, y = 0, dir = 1; 
+        int x = 0, y = 0, dir = 1; // Start facing ↑
         for (int cmd : commands) {
-            if (cmd == -1 || cmd == -2) { 
+            if (cmd == -1 || cmd == -2) { // Turn command
                 dir = changeDir(dir, cmd);
-            } else { 
+            } else { // Move command
                 int tempX = x;
                 int tempY = y;
                 for (int i = 0; i < cmd; i++) {
-                    if (dir == 1) tempY++; 
-                    else if (dir == 2) tempY--; 
-                    else if (dir == 3) tempX++; 
-                    else if (dir == 4) tempX--;
-                    
-                    if (obs.contains(tempX + "," + tempY)) break;
+                    if (dir == 1) tempY++; // Move ↑
+                    else if (dir == 2) tempY--; // Move ↓
+                    else if (dir == 3) tempX++; // Move →
+                    else if (dir == 4) tempX--; // Move ←
+
+                    if (obs.contains(tempX + "," + tempY)) break; // Stop if obstacle encountered
 
                     x = tempX;
                     y = tempY;
-                    if(((x*x) + (y*y)) > max ) 
-                        max = (x*x) + (y*y);
+                    if (((x*x) + (y*y)) > max) 
+                        max = (x*x) + (y*y); // Update max distance squared
                 }
             }
         }
