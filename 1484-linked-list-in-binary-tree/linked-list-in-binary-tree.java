ab43d746a1ca57b8fd791d13ec4c25bf30207e90
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-09-07 18:39:22 +0530

    Time: 54 ms (5.11%) | Memory: 44.9 MB (55.38%) - LeetSync

---
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.java
@@ -0,0 +1,57 @@
+class Solution {
+    List<TreeNode> heads = new ArrayList<>();
+    void headFinder(TreeNode root,int val){
+        if(root == null)
+            return;
+        if(root.val == val){
+            heads.add(root);
+        }
+        headFinder(root.left,val);
+        headFinder(root.right,val);
+    }
+    boolean ans = false;
+    void bfs(Queue<TreeNode> q,ListNode head) {
+        while (!q.isEmpty()) {
+            int len = q.size();
+            boolean found = false;
+            for (int i = 0; i < len; i++) {
+                TreeNode node = q.poll();
+                if (node == null)
+                    continue;
+                // System.out.print(" "+node.val + " : "+head.val);
+                if(head.val == node.val){
+                    found = true;
+                    if (node.left != null)
+                        q.offer(node.left);
+                    if (node.right != null)
+                        q.offer(node.right);
+                }
+                
+            }
+            System.out.println();
+            if(!found)
+                break;
+            if(head.next == null){
+                ans = true;
+                break;
+            }
+            head = head.next;
+        }
+    }
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        headFinder(root,head.val);
+        for(TreeNode n : heads){
+            q.clear();
+            q.offer(n);
+            ListNode save = new ListNode();
+            save.next = head;
+
+            bfs(q,save.next);
+            System.out.println("---------------------------");
+            if(ans)
+                return true;
+        }
+        return false;
+    }
+}
