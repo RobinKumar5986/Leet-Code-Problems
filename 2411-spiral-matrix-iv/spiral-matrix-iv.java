9cbadef7253b688ab665231dacd3c7fbacfc85b4
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-09-09 14:06:27 +0530

    Time: 7 ms (87.82%) | Memory: 64.9 MB (7.38%) - LeetSync

---
+++ 2411-spiral-matrix-iv/spiral-matrix-iv.java
@@ -0,0 +1,68 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[][] spiralMatrix(int m, int n, ListNode head) {
+        int[][] mat = new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                mat[i][j] = -1;
+            }
+        }
+        int c = 0;
+        int loi = 0 , loj = 0 , hii = m-1 , hij = n-1;
+        while(c<(m*n)){
+            for(int i = loj ;i<= hij ;i++){
+               
+                if(head!=null){
+                    mat[loi][i] = head.val;
+                    head = head.next;
+                }else{
+                    return mat;
+                }
+                c++;
+            }   
+            
+            loi++;
+            for(int i = loi ;i <= hii;i++){
+                if(head!=null){
+                    mat[i][hij] = head.val;
+                    head = head.next;
+                }else{
+                    return mat;
+                }
+                c++;
+            }
+            hij--;
+            for(int i=hij;i>=loj;i--){
+                if(head!=null){
+                    mat[hii][i] = head.val;
+                    head = head.next;
+                }else{
+                    return mat;
+                }
+                c++;
+            }
+            hii--;
+            for(int i = hii; i>=loi ; i--){
+                if(head!=null){
+                    mat[i][loj] = head.val;
+                    head = head.next;
+                }else{
+                    return mat;
+                }
+                c++;
+            }
+            loj++;
+        }
+
+        return mat;
+    }
+}
