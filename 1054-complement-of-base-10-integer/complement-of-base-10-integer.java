724264c79ace3e3e4cb29529552015a6fa60ec95
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-09-22 01:54:10 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (91.83%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -1,11 +1,17 @@
 class Solution {
     public int bitwiseComplement(int n) {
+        if(n == 0)
+            return 1;
+            
         int ans = 0;
-        String s = Integer.toBinaryString(n);
-        for(int i = 0; i < s.length() ; i++){
-            if(s.charAt(i) == '0'){
-                ans += Math.pow(2,s.length() -1 - i );
+        int pow = 0;
+        while(n > 0){
+            int t = n & 1;
+            if(t == 0){
+                ans += 1 << pow;
             }
+            pow++;
+            n = n>>1;
         }
         return ans;
     }
