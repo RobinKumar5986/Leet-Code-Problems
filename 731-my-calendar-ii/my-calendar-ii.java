788603ab0e9ba5f0d50d2f2f009ed60b9c0a860d
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-09-26 12:46:15 +0530

    Time: 170 ms (59.88%) | Memory: 45.4 MB (86.94%) - LeetSync

--- 731-my-calendar-ii/my-calendar-ii.java
+++ 731-my-calendar-ii/my-calendar-ii.java
@@ -1,36 +1,22 @@
 class MyCalendarTwo {
-    TreeMap<Integer, Integer> map = new TreeMap<>();
-
-    public MyCalendarTwo() {
+    TreeMap<Integer,Integer> map = new TreeMap<>();
+    public MyCalendarTwo() { 
     }
-
     public boolean book(int start, int end) {
         int sum = 0;
-
-        try {
-            map.put(start, map.get(start) + 1);
-        } catch (NullPointerException e) {
-            map.put(start, 1);
-        }
-
-        try {
-            map.put(end, map.get(end) - 1);
-        } catch (NullPointerException e) {
-            map.put(end, -1);
-        }
-
-        for (int ele : map.values()) {
+        map.put(start,map.getOrDefault(start,0)+1);
+        map.put(end,map.getOrDefault(end,0)-1);
+        for(int ele : map.values()){
             sum += ele;
-            if (sum >= 3) {
-                map.put(start, map.get(start) - 1);
+            if(sum >= 3){
+                map.put(start,map.get(start)-1);
                 if (map.get(start) == 0) map.remove(start);
-
-                map.put(end, map.get(end) + 1);
+                
+                map.put(end,map.get(end)+1);
                 if (map.get(end) == 0) map.remove(end);
                 return false;
             }
         }
-
         return true;
     }
 }
