e4953ded5bae29e1a20c002981d14efe14a45188
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-09-26 11:21:43 +0530

    Time: 1931 ms (5.02%) | Memory: 55.6 MB (6.52%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.java
+++ 729-my-calendar-i/my-calendar-i.java
@@ -3,21 +3,18 @@ class MyCalendar {
     public MyCalendar() {
         
     }
-    
     public boolean book(int start, int end) {
         map.put(start,map.getOrDefault(start,0)+1);
         map.put(end,map.getOrDefault(end,0)-1);
         int sum = 0;
         for(int ele : map.keySet()){
             sum += map.get(ele);
-            // System.out.println(sum + " : "+ele );
             if(sum >= 2){
                 map.put(start,map.getOrDefault(start,0)-1);
                 map.put(end,map.getOrDefault(end,0)+1);
                 return false;
             }
         }
-        // System.out.println();
         return true;
     }
 }
