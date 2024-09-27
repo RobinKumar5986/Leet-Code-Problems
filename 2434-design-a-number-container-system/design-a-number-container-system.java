a0608d8a297d9e3da6583f3ea9377b5e89229853
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-09-28 00:10:12 +0530

    Time: 89 ms (21.33%) | Memory: 97.3 MB (80.00%) - LeetSync

--- 2434-design-a-number-container-system/design-a-number-container-system.java
+++ 2434-design-a-number-container-system/design-a-number-container-system.java
@@ -1,51 +1,42 @@
-import java.util.HashMap;
-import java.util.PriorityQueue;
-import java.util.HashSet;
-
 class NumberContainers {
-    HashMap<Integer, Integer> indMap = new HashMap<>();  // Map index to number
-    HashMap<Integer, PriorityQueue<Integer>> numMap = new HashMap<>();  // Map number to min-heap of indices
-    HashMap<Integer, HashSet<Integer>> validIndices = new HashMap<>();  // Track valid indices for numbers
+    HashMap<Integer,Integer> indMap = new HashMap<>();
+    HashMap<Integer,TreeSet<Integer>> numMap = new HashMap<>();
 
     public NumberContainers() {
+        
     }
-
+    
     public void change(int index, int number) {
-        // If the index already has a number, remove it from the old number's heap
-        if (indMap.containsKey(index)) {
-            int oldNumber = indMap.get(index);
-            if (numMap.containsKey(oldNumber)) {
-                // Mark the old index as invalid for the old number
-                validIndices.get(oldNumber).remove(index);
+        Integer key = null;
+        if(indMap.containsKey(index)){
+            key = indMap.get(index);
+        }
+        indMap.put(index , number);
+
+        if(key != null){
+            TreeSet<Integer> set = numMap.get(key);
+            set.remove(index);
+            if(set.isEmpty()){
+                numMap.remove(key);
+            }else{
+                numMap.put(key,set);
             }
         }
 
-        // Update the index with the new number
-        indMap.put(index, number);
+        if(numMap.get(number) == null){
+            numMap.put(number,new TreeSet<>());
+        }
+        TreeSet<Integer> set = numMap.get(number);
+        set.add(index);
 
-        // Add the index to the heap for the new number
-        numMap.computeIfAbsent(number, k -> new PriorityQueue<>()).add(index);
-        validIndices.computeIfAbsent(number, k -> new HashSet<>()).add(index);
+        numMap.put(number,set);
     }
-
+    
     public int find(int number) {
-        // If the number is not present, return -1
-        if (!numMap.containsKey(number) || numMap.get(number).isEmpty()) {
+        if(!numMap.containsKey(number)){
             return -1;
         }
-
-        // Clean up the heap (remove invalid indices that have been changed)
-        PriorityQueue<Integer> heap = numMap.get(number);
-        while (!heap.isEmpty() && !validIndices.get(number).contains(heap.peek())) {
-            heap.poll();  // Remove invalid indices from the heap
-        }
-
-        // If the heap is empty after cleanup, return -1
-        if (heap.isEmpty()) {
-            return -1;
-        }
-
-        // Return the smallest valid index for this number
-        return heap.peek();
+        
+        return numMap.get(number).first();
     }
 }
