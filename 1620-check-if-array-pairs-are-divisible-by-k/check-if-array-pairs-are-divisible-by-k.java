9da27d645ae00575ce0cc10035f2e57f8c3728b3
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-10-02 09:34:53 +0530

    Time: 67 ms (5.20%) | Memory: 60.7 MB (5.41%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.java
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.java
@@ -1,33 +1,32 @@
 class Solution {
     public boolean canArrange(int[] arr, int k) {
         HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i = 0 ; i < arr.length ; i++){
+        for(int i = 0; i<arr.length ; i++){
             int rem = arr[i] % k;
             if(rem < 0){
                 rem += k;
             }
             arr[i] = rem;
-            map.put(rem, map.getOrDefault(rem,0) + 1);
+            map.put(rem , map.getOrDefault(rem,0)+1);
         }
-        int ind = 0;
         int c = 0;
-        Arrays.sort(arr);
-        while(ind < arr.length  && arr[ind] == 0){
-            ind++;
-            c++;
+
+        for(int ele : arr){
+            if(ele == 0){
+                c++;
+                continue;
+            }
+            int req = (k - ele);
+            if(!map.containsKey(req))
+                return false;
+            map.put(req, map.get(req) - 1);
+            if(map.get(req)==0)
+                map.remove(req);
         }
         if(c % 2 != 0)
             return false;
-        if((arr.length - ind) % 2 != 0 )
+        if((arr.length - c) % 2 != 0 )
             return false;
-        int lo = ind;
-        int hi = arr.length -1;
-        while(lo < hi ){
-            if(arr[lo] + arr[hi] != k)
-                return false;
-            lo++;
-            hi--;
-        }
         return true;
     }
 }
