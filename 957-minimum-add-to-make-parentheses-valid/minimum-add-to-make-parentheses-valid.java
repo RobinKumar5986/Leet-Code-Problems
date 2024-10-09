e41387ae5c361c9bc81f1c6302e8db711cf2c863
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-10-10 00:16:18 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (90.31%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -3,7 +3,6 @@ class Solution {
         int ans = 0;
         int c = 0;
         for(char ch : s.toCharArray()){
-           
             if(ch == ')'){
                 if(c == 0)
                     ans++;
