59061ffe191bf10daabc13cecb0d3a1de6fe7dc8
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-10-16 01:24:11 +0530

    Time: 9 ms (22.30%) | Memory: 45.5 MB (41.73%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
@@ -1,22 +1,22 @@
 class Solution {
     public long minimumSteps(String s) {
         long ans = 0;
-        int ind = -1;
-        int i = 0; 
-        for (char c : s.toCharArray()) {
-            if (ind == -1 && c == '0') {
-                i++;
-                continue;
-            }
+        int ind = -1; // Keeps track of the index of the first '1'
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
             if (c == '1' && ind == -1) {
-                ind = i;
+                ind = i; // Record the position of the first '1'
             }
-            if (c == '0') {
+            
+            // If we have seen '1' before and encounter a '0', calculate the steps
+            if (ind != -1 && c == '0') {
                 ans += i - ind;
-                ind++;
+                ind++; // Move to the next '1' after processing this '0'
             }
-            i++;
         }
+        
         return ans;
     }
 }
