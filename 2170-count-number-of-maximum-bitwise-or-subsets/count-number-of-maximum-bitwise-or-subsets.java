686cea82aaab8bc229d5e11c4233dfa4440b75f4
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-10-19 02:18:53 +0530

    Time: 12 ms (49.43%) | Memory: 44.7 MB (28.98%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
@@ -1,21 +1,51 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     int max = 0;
-    int c = 0;
-    void subset(int[] nums , int cur , int ind){
-        if(ind == nums.length){
-            if(cur == max)
-                c++;
+    int count = 0;
+    Map<String, Integer> memo = new HashMap<>();
+
+    void subset(int[] nums, int cur, int ind) {
+        // Prune: If current OR value already equals max, no need to continue
+        if (cur == max) {
+            count += (1 << (nums.length - ind));  // Count all remaining subsets
             return;
         }
-        //not include
-        subset(nums , cur , ind+1);
-        //include
-        subset(nums , cur | nums[ind], ind+1);
+        
+        // If we've reached the end of the array
+        if (ind == nums.length) {
+            if (cur == max) count++;
+            return;
+        }
+
+        // Memoization to avoid repeated calculations
+        String key = cur + "," + ind;
+        if (memo.containsKey(key)) {
+            count += memo.get(key);
+            return;
+        }
+
+        int initialCount = count;
+
+        // Option 1: Do not include the current element
+        subset(nums, cur, ind + 1);
+        
+        // Option 2: Include the current element
+        subset(nums, cur | nums[ind], ind + 1);
+
+        // Store the result in memo
+        memo.put(key, count - initialCount);
     }
+
     public int countMaxOrSubsets(int[] nums) {
-        for(int ele : nums)
+        // Compute the maximum possible OR value
+        for (int ele : nums)
             max = max | ele;
-        subset(nums,0,0);
-        return c;
+        
+        // Start the subset generation and counting
+        subset(nums, 0, 0);
+        
+        return count;
     }
 }
