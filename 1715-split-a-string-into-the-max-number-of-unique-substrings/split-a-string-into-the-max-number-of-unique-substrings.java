d0b200d6920b7cecdaa7cfd5eeb1f00599a08721
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-10-22 00:01:42 +0530

    Time: 32 ms (77.78%) | Memory: 45.6 MB (6.54%) - LeetSync

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.java
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.java
@@ -3,14 +3,11 @@ class Solution {
     int len = 0;
     void tracker(int ind ,String s, Set<String> set){
         if( ind >= len){
-            if(set.size() > max){
-                max = set.size();
-            }
+            max = Math.max(max, set.size());
             return;
         }
-        String str = "";
         for(int i = ind ; i < len ; i++){
-            str += s.charAt(i);
+            String str = s.substring(ind, i + 1);
             if(!set.contains(str)){
                 set.add(str);
                 tracker(i+1,s,set);
