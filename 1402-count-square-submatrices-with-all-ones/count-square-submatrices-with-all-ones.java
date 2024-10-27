472e9fee09eac95eba9db2faff9b6fd1be26a3f5
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-10-28 01:36:30 +0530

    Time: 82 ms (9.68%) | Memory: 56.1 MB (22.27%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -22,8 +22,6 @@ class Solution {
         int ans = 0;
         for(int i = 0 ; i < mat.length ; i++){
             for(int j = 0 ; j< mat[0].length; j++){
-                if(mat[i][j] == 0)
-                    continue;
                 ans += dfs(i,j,mat);
             }
         }
