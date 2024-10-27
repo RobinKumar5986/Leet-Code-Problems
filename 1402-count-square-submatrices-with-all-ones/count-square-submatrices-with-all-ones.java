a873981a0739a9b8a4d778a306014cda77bfa65d
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-10-28 01:42:26 +0530

    Time: 7 ms (47.31%) | Memory: 55.2 MB (38.33%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -1,30 +1,20 @@
 class Solution {
-    HashMap<String,Integer> map = new HashMap<>();
-    int dfs(int r , int c , int[][] mat){
-        if(r >= mat.length || c >= mat[0].length || mat[r][c] == 0){
-            return 0;
-        }
-        String s = r+","+c;
-        if(map.containsKey(s))
-            return map.get(s);
-
-        int len = 1;
-        int s1 = dfs(r+1,c,mat);
-        int s2 = dfs(r,c+1,mat);
-        int s3 = dfs(r+1,c+1 , mat);
-
-        len += Math.min(s1,Math.min(s2,s3));
-        map.put(s,len);
-
-        return len; 
-    }
     public int countSquares(int[][] mat) {
+        int[][] dp = new int[mat.length][mat[0].length];
         int ans = 0;
         for(int i = 0 ; i < mat.length ; i++){
-            for(int j = 0 ; j< mat[0].length; j++){
-                ans += dfs(i,j,mat);
+            for(int j = 0 ; j < mat[0].length ; j++ ){
+                if(mat[i][j] == 1){
+                    if(i > 0 && j > 0){
+                        dp[i][j]= Math.min(dp[i-1][j] , Math.min(dp[i][j-1] , dp[i-1][j-1])) + 1;
+                    }else{
+                        dp[i][j] = 1;
+                    }
+                }
+                ans += dp[i][j];
             }
         }
         return ans;
+
     }
 }
