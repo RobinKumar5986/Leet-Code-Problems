13b178979b500cff37f1cf45126be4e45a11a225
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-11-03 23:41:26 +0530

    Time: 4 ms (50.61%) | Memory: 49.1 MB (78.80%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,21 +1,21 @@
 class Solution {
     boolean[][] dp;
     void expand(int i , int j , char[][] grid){
-        dp[i][j] = true;
-
-        if(i + 1 < grid.length && !dp[i+1][j] && grid[i+1][j] == '1'){
-            expand(i+1,j,grid);
+        if (
+        i >= grid.length 
+        || i < 0 
+        || j >= grid[0].length || j < 0 
+        || grid[i][j] == '0' 
+        || dp[i][j]
+        ) {
+            return;
         }
-
-        if(i-1 >= 0  && !dp[i-1][j] && grid[i-1][j] == '1')
-            expand(i-1,j,grid);
-
-        if(j + 1 < grid[0].length && !dp[i][j+1] && grid[i][j+1] == '1')
-            expand(i,j+1,grid);
-
-        if(j-1 >= 0  && !dp[i][j-1] && grid[i][j-1] == '1')
-            expand(i,j-1,grid);
         
+        dp[i][j] = true;
+        expand(i + 1, j, grid);
+        expand(i - 1, j, grid);
+        expand(i, j + 1, grid);
+        expand(i, j - 1, grid);
     }
     public int numIslands(char[][] grid) {
         dp = new boolean[grid.length][grid[0].length];
@@ -27,8 +27,7 @@ class Solution {
                 expand(i,j,grid);
                 ans++;
             }
-        } 
-
+        }
         return ans;
     }
 }
