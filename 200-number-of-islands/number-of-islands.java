f6b335165ad5f592d8ff2a29a890bc2ccd69b5b5
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-11-03 23:09:45 +0530

    Time: 140 ms (5.38%) | Memory: 52.8 MB (5.87%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.java
@@ -0,0 +1,35 @@
+class Solution {
+    boolean[][] dp;
+    void expand(int i , int j , char[][] grid){
+        System.out.println(i + " : "+ j);
+        dp[i][j] = true;
+
+        if(i + 1 < grid.length && !dp[i+1][j] && grid[i+1][j] == '1'){
+            expand(i+1,j,grid);
+        }
+
+        if(i-1 >= 0  && !dp[i-1][j] && grid[i-1][j] == '1')
+            expand(i-1,j,grid);
+
+        if(j + 1 < grid[0].length && !dp[i][j+1] && grid[i][j+1] == '1')
+            expand(i,j+1,grid);
+
+        if(j-1 >= 0  && !dp[i][j-1] && grid[i][j-1] == '1')
+            expand(i,j-1,grid);
+        
+    }
+    public int numIslands(char[][] grid) {
+        dp = new boolean[grid.length][grid[0].length];
+        int ans = 0;
+        for(int i = 0 ; i < grid.length ; i++){
+            for(int j = 0 ; j < grid[0].length; j++ ){
+                if(grid[i][j] == '0' || dp[i][j])
+                    continue;
+                expand(i,j,grid);
+                ans++;
+            }
+        } 
+
+        return ans;
+    }
+}
