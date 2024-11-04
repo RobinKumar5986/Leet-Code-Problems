77fb5a2a92c893e3d3c5143ffcb20587640fe03e
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-11-04 12:07:12 +0530

    Time: 18 ms (66.10%) | Memory: 45.7 MB (68.96%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.java
+++ 3451-string-compression-iii/string-compression-iii.java
@@ -6,23 +6,27 @@ class Solution {
         for(int i = 1 ; i < s.length() ; i++){
             if( co > 9 ){
                 co = 1;
-                sb.append("9" + c );
+                sb.append('9');
+                sb.append(c);
             }
             if( c == s.charAt(i)){
                 co++;
             }
             else{
-                sb.append(co + "" + c);
+                sb.append(co);
+                sb.append(c);
                 c = s.charAt(i);
                 co = 1;
             }
         }
         while(co > 9){
-            sb.append("9" + c);
+            sb.append('9');
+            sb.append(c);
             co -= 9;
         }
         if(co > 0){
-            sb.append(co + "" + c);
+            sb.append(co);
+            sb.append(c);
         }
         return sb.toString();
     }
