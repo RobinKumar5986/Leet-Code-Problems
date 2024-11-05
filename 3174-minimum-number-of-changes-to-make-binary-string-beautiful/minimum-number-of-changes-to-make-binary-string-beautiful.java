6fefb653bb633c92fccf2a38d392a2bbc90bf22d
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-11-06 00:12:54 +0530

    Time: 3 ms (97.78%) | Memory: 44.1 MB (99.22%) - LeetSync

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.java
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.java
@@ -1,12 +1,11 @@
 class Solution {
     public int minChanges(String s) {
-        int ch = 0;
-        for(int i = 0 ; i < s.length() ; i+=2){
-            char c1 = s.charAt(i);
-            char c2 = s.charAt(i+1);
-            if(c1 != c2)
-                ch++;
+        int changes = 0;
+        for (int i = 0; i < s.length() - 1; i += 2) {
+            if (s.charAt(i) != s.charAt(i + 1)) {
+                changes++;
+            }
         }
-        return ch;
+        return changes;
     }
 }
