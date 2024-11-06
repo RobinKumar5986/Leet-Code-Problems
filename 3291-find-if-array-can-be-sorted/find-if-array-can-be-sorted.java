0d8ddb3bfb6dcc5017d7ec5cb509bc3f3dd56351
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-11-06 13:36:24 +0530

    Time: 52 ms (5.71%) | Memory: 44.9 MB (6.92%) - LeetSync

---
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean canSortArray(int[] nums) {
+        for(int i = 0 ; i<nums.length ; i++){
+            for(int j = 0;j<nums.length-1;j++){
+                if(nums[j] > nums[j+1]){
+                    int set1 = 0;
+                    int set2 = 0;
+                    int t1 = nums[j];
+                    int t2 = nums[j+1];
+                    while(t1 != 0){
+                        set1 += t1 & 1;
+                        t1 >>= 1;
+                    }
+                    while(t2 != 0){
+                        set2 += t2 & 1;
+                        t2 >>= 1;
+                    }
+                    if(set1 != set2)
+                        return false;
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+            }
+        }
+        for(int ele : nums){
+            System.out.print(ele + " ");
+        }
+        return true;
+    }
+}
