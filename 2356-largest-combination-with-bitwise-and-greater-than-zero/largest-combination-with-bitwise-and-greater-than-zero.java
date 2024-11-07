88741ad618ae10c58d934d65625c17a5588b0dea
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-11-08 00:56:15 +0530

    Time: 28 ms (95.24%) | Memory: 56.9 MB (27.09%) - LeetSync

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.java
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.java
@@ -1,14 +1,15 @@
+import java.math.BigInteger;
 class Solution {
     public int largestCombination(int[] candi) {
         int ans = 0;
         int max = Integer.MIN_VALUE;
         for(int ele : candi)
             max = Math.max(ele,max);
-        String bin = Integer.toBinaryString(max);
-        int[] bitMap = new int[bin.length()];
+        int bitLength = BigInteger.valueOf(max).bitLength();
+        int[] bitMap = new int[bitLength];
 
         for(int ele : candi){
-            int ind = bin.length()-1;
+            int ind = bitLength - 1;
             while(ele != 0){
                 int bit = ele & 1;
                 if(bit == 1){
