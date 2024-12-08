1edf8c2b9a79092f8321b0c545751b3ae2991a3a
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-12-08 17:18:32 +0530

    Time: 85 ms (10.49%) | Memory: 60 MB (76.65%) - LeetSync

--- 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.java
+++ 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.java
@@ -1,27 +1,27 @@
 class Solution {
-    boolean isOk(int n , int[] nums , int mOp){
-        int total = 0;
-        for(int num : nums){
-            int rem = (int)Math.ceil((double)num/n) - 1;
-            total += rem;
-            if(total > mOp)
+    boolean isOk(int n , int[] nums,int mOp){
+        int sum = 0;
+        for(int ele: nums){
+            int rem = (int)Math.ceil((double)ele/n) - 1;
+            sum += rem;
+            if(sum > mOp)
                 return false;
         }
         return true;
     }
     public int minimumSize(int[] nums, int mOp) {
         int hi = -1;
-        for(int ele : nums)
-            hi = Math.max(ele,hi);
+        for(int ele : nums )
+            hi = Math.max(hi,ele);
         int ans = hi;
         int lo = 1;
         while(lo <= hi){
-            int mid = (lo+hi)/2;
+            int mid = lo + (hi - lo) / 2;
             if(isOk(mid,nums,mOp)){
-                ans = Math.min(ans,mid);
+                ans = mid;
                 hi = mid - 1;
             }else{
-                lo = mid+1;
+                lo = mid + 1;
             }
         }
         return ans;
