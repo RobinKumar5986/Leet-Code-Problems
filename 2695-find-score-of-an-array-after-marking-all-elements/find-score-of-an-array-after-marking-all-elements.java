9664aa341fd91d3fd023dc8f59d9c43bd282cf83
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2024-12-13 19:37:33 +0530

    Time: 157 ms (57.35%) | Memory: 68.8 MB (5.09%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
@@ -1,25 +1,29 @@
 class Solution {
     public long findScore(int[] nums) {
         long ans = 0;
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> {
+        int[][] pair = new int[nums.length][2];
+        for(int i = 0 ; i < nums.length ; i++){
+            pair[i][0] = nums[i];
+            pair[i][1] = i;
+        }
+        Arrays.sort(pair , (a,b) -> {
             if(a[0] != b[0]){
                 return Integer.compare(a[0],b[0]);
             }
-            return Integer.compare(a[1],b[1]);
+            return Integer.compare(a[1], b[1]);
         });
-        for(int i = 0 ; i < nums.length; i++){
-            minHeap.offer(new int[]{nums[i],i});
-        }
+
         Set<Integer> vis = new HashSet<>();
-        while (!minHeap.isEmpty()) {
-            int[] pair = minHeap.poll();
-            if(!vis.contains(pair[1])){
-                ans += pair[0];
-                vis.add(pair[1]);
-                vis.add(pair[1]+1);
-                vis.add(pair[1]-1);
+        for(int i = 0 ; i < nums.length; i++){
+            if(!vis.contains(pair[i][1])){
+                ans+= pair[i][0];
+                vis.add(pair[i][1]);
+                vis.add(pair[i][1] + 1);
+                vis.add(pair[i][1]-1);
             }
         }
+        
+        
         return ans;
     }
 }
