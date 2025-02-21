81569874fd1ad14edd678c674d0caad773679419
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2025-02-21 13:03:13 +0530

    Time: 20 ms (98.92%) | Memory: 47.3 MB (70.08%) - LeetSync

--- 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.java
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.java
@@ -1,26 +1,26 @@
 class FindElements {
-    Set<Integer> set = new HashSet<>();
+    BitSet set = new BitSet();
     void recover(TreeNode st,int parent){
         if(st == null)
             return;
         if(st.left != null){
             st.left.val = parent * 2 + 1;
-            set.add(st.left.val);
+            set.set(st.left.val);
             recover(st.left,st.left.val);
         }
         if(st.right != null){
             st.right.val = parent * 2 + 2;
-            set.add(st.right.val);
+            set.set(st.right.val);
             recover(st.right,st.right.val);
         }
     }
     public FindElements(TreeNode root) {
-        set.add(0);
+        set.set(0);
         root.val = 0;
         recover(root,0);
     }
     
     public boolean find(int target) {
-        return set.contains(target);
+        return set.get(target);
     }
 }
