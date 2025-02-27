4f3ad6b5984000330dd47f10eaeadbfb6982930a
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2025-02-28 01:21:23 +0530

    Time: 172 ms (56.81%) | Memory: 44.9 MB (92.75%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.java
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.java
@@ -1,15 +1,15 @@
 class Solution {
     public int lenLongestFibSubseq(int[] arr) {
-        HashMap<Integer, Integer> map = new HashMap<>(); // For lookup
+        Set<Integer> set = new HashSet<>(); // For lookup
         for (int i = 0; i < arr.length; i++) {
-            map.put(arr[i], i);
+            set.add(arr[i]);
         }
         int ans = 0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 int prev = arr[i], cur = arr[j];
                 int co = 2;
-                while (map.containsKey(prev + cur)) {
+                while (set.contains(prev + cur)) {
                     int key = prev + cur;
                     prev = cur;
                     cur = key;
