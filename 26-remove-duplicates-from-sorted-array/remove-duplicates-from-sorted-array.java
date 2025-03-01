703fdf0136b963e4cec578678c7ebe8b3336f4ab
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2025-03-01 12:50:10 +0530

    Time: 1 ms (66.79%) | Memory: 44.8 MB (77.34%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -3,14 +3,11 @@ class Solution {
         int unique[] = new int[nums.length];
         int co = 0;
         for(int i = 0 ; i < nums.length; i++){
-            unique[co] = nums[i];
+            nums[co] = nums[i];
             co++;
             while(i+1 < nums.length && nums[i] == nums[i+1]){
                 i++;
             }
-        }
-        for(int i = 0 ; i <co ; i++){
-            nums[i] = unique[i];
         }
         return co;
     }
