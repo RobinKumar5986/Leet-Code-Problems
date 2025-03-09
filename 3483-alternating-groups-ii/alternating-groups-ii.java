c268ff96f19adc083b5690dfbd6590465ae6bb53
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2025-03-10 01:15:25 +0530

    Time: 4 ms (68.25%) | Memory: 63.3 MB (37.33%) - LeetSync

---
+++ 3483-alternating-groups-ii/alternating-groups-ii.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int numberOfAlternatingGroups(int[] col, int k) {
+        int issue = -1;
+        for(int i = 1; i < k; i++){
+            if(col[i-1] == col[i])
+                issue = i-1;
+        }
+        int ans = 0;
+        if(issue == -1) ans++;
+
+        int lo = 1;
+        int hi = k;
+        while(lo < col.length){
+            hi = hi % col.length;
+            int prev = hi -1;
+            if(hi == 0){
+                prev = col.length-1;
+            }
+            if(col[prev] == col[hi]){
+                issue = prev;
+            }
+            // System.out.println(lo + " : "+ hi + " : "+ issue + " : "+ ans);
+            if(issue == -1){
+                ans++;
+            }else{
+                if( hi < lo){
+                    if(issue >= hi && issue < lo)
+                        ans++;
+                }else{
+                    if(issue < lo )
+                        ans++;
+                }
+            }
+            lo++;
+            hi++;
+        }
+        return ans;
+    }
+}
