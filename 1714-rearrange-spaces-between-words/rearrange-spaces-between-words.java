6a63f31c43f0296b4e345e9c2452af04e38b8c14
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2025-03-11 13:19:02 +0530

    Time: 6 ms (12.05%) | Memory: 42.2 MB (11.07%) - LeetSync

--- 1714-rearrange-spaces-between-words/rearrange-spaces-between-words.java
+++ 1714-rearrange-spaces-between-words/rearrange-spaces-between-words.java
@@ -19,27 +19,24 @@ class Solution {
         }
         if(sp == 0)
             return text;
-        StringBuilder sb = new StringBuilder();
-        String space = "";
-        if(words.size()-1 != 0){
-            for(int i = 1 ; i<= sp/(words.size()-1) ; i++){
-                space += " ";
-            }
-        }
-        for(int i = 0; i < words.size(); i++){
-            sb.append(words.get(i));
-            if(i <  words.size() -1){
-                sb.append(space);
-            }
-        }
+        StringBuilder ans = new StringBuilder();
         if(words.size() == 1){
-            for(int i = 0 ; i < sp ; i++)
-                sb.append(" ");
-        }else{
-            int rem = sp % (words.size()-1);
-            for(int i = 0 ; i < rem ; i++)
-                sb.append(" ");
+            ans.append( words.get(0) );
+            for(int i = 0 ; i < sp; i++)
+                ans.append( " " );
+            return ans.toString();
         }
-        return sb.toString();
+        String space = "";
+        for(int i = 1; i<= sp / (words.size() -1) ; i++)
+            space+= " ";
+        for(int i = 0 ; i < words.size(); i++){
+            ans.append(words.get(i));
+            if(i < words.size()-1)
+                ans.append(space);
+        }
+        int rem = sp % (words.size() - 1);
+        for(int i = 0 ; i < rem ;i++)
+            ans.append(" ");
+        return ans.toString();
     }
 }
