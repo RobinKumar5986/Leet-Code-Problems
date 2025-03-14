3a0f4dad9f2abd1d60f01a325d1db2a533dd5be8
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2025-03-15 02:12:16 +0530

    Time: 13 ms (95.88%) | Memory: 63.5 MB (22.34%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
@@ -1,30 +1,28 @@
-class Solution {     
-    boolean helper(int[] candies, long k, long pile) {
-        if (pile == 0) return false;
+class Solution {
+    boolean helper(int[] candies, long k ,long pile){
+        if(pile == 0 ) return false;
         long count = 0;
-        for (int ele : candies) {
+        for(int ele : candies){
             count += ele / pile;
-            if (count >= k) return true;
+            if(count >= k)
+                return true;
         }
         return false;
     }
-
     public int maximumCandies(int[] candies, long k) {
         long sum = 0;
-        for (int ele : candies) sum += ele;
-
-        if (sum < k) return 0;
-
-        long lo = 1, hi = sum / k;
+        for(int ele : candies)
+            sum += ele;
+        long lo = 1;
+        long hi = sum / k;
         int ans = 0;
-
-        while (lo <= hi) {
+        while(lo <= hi){
             long mid = lo + (hi - lo) / 2; 
-            if (helper(candies, k, mid)) {
-                ans = (int) mid;
-                lo = mid + 1;
-            } else {
-                hi = mid - 1;
+            if(helper(candies,k,mid)){
+                lo = mid+1;
+                ans = (int)mid;
+            }else{
+                hi = mid-1;
             }
         }
         return ans;
