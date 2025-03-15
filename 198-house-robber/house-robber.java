10389c17cceee83c33862bbc931e286fabb47b03
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2025-03-15 18:32:03 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (23.87%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,19 @@
+class Solution {
+    int[] dp;
+
+    int sol(int[] nums,int i){
+        if(i < 0 || i>= nums.length)
+            return 0;
+        if(dp[i] != -1)
+            return dp[i];
+        dp[i] = Math.max(nums[i] + sol(nums,i-2), sol(nums,i-1) );
+
+        return dp[i];
+    }
+    public int rob(int[] nums) {
+        dp = new int[nums.length];
+        for(int i = 0 ; i < nums.length; i++)
+            dp[i] = -1;
+        return sol(nums,nums.length-1);
+    }
+}
