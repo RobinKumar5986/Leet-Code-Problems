9215e4a0e46be689fd9f825e520df494fd3f5f9d
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2025-03-18 11:28:50 +0530

    Time: 3 ms (98.64%) | Memory: 55.7 MB (67.85%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.java
+++ 2478-longest-nice-subarray/longest-nice-subarray.java
@@ -1,18 +1,19 @@
 class Solution {
     public int longestNiceSubarray(int[] nums) {
+        //all the set bit should be match and should be opposit 
         if(nums.length <= 1) return 1;
         int lo = 0;
         int hi = 1;
-        int ans = 1;
         int mask = nums[0];
+        int ans = 1;
         while(hi < nums.length){
             while((mask & nums[hi]) != 0){
                 mask ^= nums[lo];
                 lo++;
             }
             mask |= nums[hi];
-            ans = Math.max(ans,hi - lo + 1);
-            hi++;
+            ans = Math.max(ans,hi-lo+1);
+            hi++; 
         }
         return ans;
     }
