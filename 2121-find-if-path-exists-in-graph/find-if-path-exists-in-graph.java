3b80b87f5f1b472f62a6222d366293b3765c43a7
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2025-03-22 13:02:14 +0530

    Time: 210 ms (28.59%) | Memory: 197.3 MB (15.01%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,55 +1,52 @@
 class Solution {
+    //Map for all the neighbours of the node.
+    Map<Integer,List<Integer>> map = new HashMap<>();
     Set<Integer> vis = new HashSet<>();
-    Map<Integer,Set<Integer>> map = new HashMap<>();
     boolean ans = false;
-    void bfs(Queue<Integer> stk,int dst){
-        if(stk.isEmpty() || ans){
+    void bfs(Queue<Integer> q, int dest){
+        if(q.isEmpty() || ans == true)
             return;
-        }
-        int sz = stk.size();
-        for(int i = 0 ; i < sz ; i ++){
-            int node = stk.remove();
-            if(node == dst){
+        int sz = q.size();
+        for(int i = 0 ; i < sz ; i++){
+            int node = q.remove();
+            if(node == dest){
                 ans = true;
                 return;
             }
-            if(!vis.contains(node)){
-            Set<Integer> nn = map.get(node);
-                for(int ele : nn){
-                    if(!vis.contains(ele)){
-                        stk.offer(ele);
-                    }
+            List<Integer> neg = map.get(node);
+            for(int ele : neg){
+                if(ele == dest){
+                    ans = true;
+                    return;
+                }
+                if(!vis.contains(ele)){
+                    q.offer(ele);
+                    vis.add(ele);
                 }
             }
-            vis.add(node);
         }
-        bfs(stk,dst);
+        bfs(q,dest);
+     
     }
-    public boolean validPath(int n, int[][] edges, int src, int dst) {
-        if(src == dst)
-            return true;
-       
-        for(int[] ele : edges){
-            if(!map.containsKey(ele[0])){
-                map.put(ele[0],new HashSet<>());
-            }
-            Set<Integer> set1 = map.get(ele[0]);
-            set1.add(ele[1]);
-            map.put(ele[0],set1);
-
-            if(!map.containsKey(ele[1])){
-                map.put(ele[1],new HashSet<>());
-            }
-            Set<Integer> set2 = map.get(ele[1]);
-            set2.add(ele[0]);
-            map.put(ele[1],set2);
-        }
-        Queue<Integer> stk = new LinkedList<>();
-        Set<Integer> neb = map.get(src);
-        for(int ele : neb){
-            stk.add(ele);
-        }
-        bfs(stk,dst);
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        Queue<Integer> q = new LinkedList<>();
+        map(edges);
+        vis.add(source);
+        q.offer(source);
+        int x = 0;
+        bfs(q,destination);
         return ans;
+    }
+    void map(int[][] edges){
+         for(int[] ele : edges){
+            if(!map.containsKey(ele[0]))
+                map.put(ele[0], new ArrayList<>());
+            if(!map.containsKey(ele[1]))
+                map.put(ele[1],new ArrayList<>());
+            List<Integer> l1 = map.get(ele[0]);
+            l1.add(ele[1]);
+            List<Integer> l2 = map.get(ele[1]);
+            l2.add(ele[0]);   
+        }
     }
 }
