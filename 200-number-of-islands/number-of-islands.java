8c3a4eba52d5334096a06340537b1ff5b9c299cf
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2025-04-20 12:56:27 +0530

    Time: 4 ms (49.30%) | Memory: 49.5 MB (70.89%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,22 +1,23 @@
 class Solution {
     boolean[][] vis;
     void expand(char[][] grid,int row,int col){
-        
+        if(vis[row][col])
+            return;
         vis[row][col] = true;
         //expandin up
-        if(row - 1 >= 0 && grid[row-1][col] == '1' && !vis[row-1][col]){
+        if(row - 1 >= 0 && grid[row-1][col] == '1' ){
             expand(grid,row-1,col);
         }
         //expanding down
-        if(row + 1 < grid.length && grid[row+1][col] == '1' && !vis[row+1][col]){
+        if(row + 1 < grid.length && grid[row+1][col] == '1' ){
             expand(grid,row+1,col);
         }
         //expanding right
-        if(col + 1 < grid[0].length && grid[row][col+1] == '1' && !vis[row][col+1]){
+        if(col + 1 < grid[0].length && grid[row][col+1] == '1' ){
             expand(grid,row,col+1);
         }
         //expanding left
-        if(col - 1 >= 0 && grid[row][col-1] == '1' && !vis[row][col-1]){
+        if(col - 1 >= 0 && grid[row][col-1] == '1'){
             expand(grid,row,col-1);
         }
     }
