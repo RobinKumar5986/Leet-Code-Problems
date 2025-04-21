caa6b0b434211352305448a071067991f192d90d
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2025-04-21 15:56:27 +0530

    Time: 25 ms (69.60%) | Memory: 42.8 MB (74.20%) - LeetSync

--- 133-clone-graph/clone-graph.java
+++ 133-clone-graph/clone-graph.java
@@ -1,20 +1,19 @@
-
 class Solution {
-    HashMap<Node,Node> map = new HashMap<>();
-    Node dfs(Node node){
-        if(map.containsKey(node))
-            return map.get(node);
-        Node copy = new Node(node.val);
-
-        map.put(node,copy);
-        for(Node neg : node.neighbors){
-            copy.neighbors.add(dfs(neg));
+    Map<Node,Node> map = new HashMap<>();
+    Node dfs(Node old){
+        if(map.containsKey(old))
+            return map.get(old);
+        Node newNode = new Node(old.val);
+        map.put(old,newNode);
+        //now need to copy the neg
+        for(Node neg : old.neighbors){
+            newNode.neighbors.add(dfs(neg));
         }
-        return copy;
+        return newNode;
     }
     public Node cloneGraph(Node node) {
         if(node == null)
             return null;
-        return dfs(node); 
+        return dfs(node);
     }
 }
