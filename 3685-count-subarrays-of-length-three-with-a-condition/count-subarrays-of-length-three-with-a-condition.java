5df867a3cfddefe55b2855a4256788d9618ae887
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2025-04-27 17:16:07 +0530

    Time: 1 ms (100.00%) | Memory: 45.2 MB (29.98%) - LeetSync

--- 3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.java
+++ 3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.java
@@ -3,13 +3,13 @@ class Solution {
         int lo = 0;
         int hi = 2;
         int ans = 0;
-        while( hi < nums.length){
-            int mid = nums[lo+1]/2;
-            if(nums[lo+1]%2 != 0){
+        while(hi < nums.length){
+            if(nums[lo+1] % 2 != 0){
                 lo++;
                 hi++;
                 continue;
             }
+            int mid = nums[lo+1]/2;
             if(nums[lo] + nums[hi] == mid)
                 ans++;
             lo++;
