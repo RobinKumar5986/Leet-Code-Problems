568c7c1ef786e4f397c32c7ae5f6978d7f4b6b31
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2025-07-15 01:23:34 +0530

    Time: 3 ms (72.92%) | Memory: 43.2 MB (69.99%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        int lo = 0;
+        int hi = s.length() - 1;
+        //lets do this in but complex way but it will be more time efficient and one pass sol
+        //insted of two pass solution 
+        while( lo < hi ){
+            while(lo < s.length() && (s.charAt(lo) < 'a' || s.charAt(lo) > 'z') && (s.charAt(lo) < '0' || s.charAt(lo) > '9')){
+                lo++;
+            }
+            if(lo >= s.length())
+                return true;
+            while(hi >= 0 && (s.charAt(hi) < 'a' || s.charAt(hi) > 'z') && (s.charAt(hi) < '0' || s.charAt(hi) > '9')){
+                hi--;
+            }
+            
+            if(hi < 0)
+                return true;
+            
+            if(s.charAt(lo) != s.charAt(hi))
+                return false;
+            lo++;
+            hi--;
+        }
+        return true;
+    }
+}
