71701c9f36f811918d4448132b7ef652cb0e70c6
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2025-07-16 01:18:09 +0530

    Time: 1437 ms (6.43%) | Memory: 55.5 MB (5.50%) - LeetSync

---
+++ 15-3sum/3sum.java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        Set<List<Integer>> ans = new HashSet<>();
+
+        for(int i = 0 ; i < nums.length ; i++) {
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i] , new ArrayList<>());
+            }
+            List<Integer> set = map.get(nums[i]);
+            set.add(i);
+            map.put(nums[i],set);
+        }
+        for(int i = 0 ; i < nums.length; i++){
+            int target = 0 - nums[i];
+            for(int j = i+1 ; j < nums.length; j++){
+                int req = target - nums[j];
+                if(map.containsKey(req)){
+                    List<Integer> inds = map.get(req);
+                    List<Integer> trip = new ArrayList<>();
+                    if(inds.size() >= 3){
+                        trip.add(nums[i]);
+                        trip.add(nums[j]);
+                        trip.add(nums[inds.get(0)]);
+                        Collections.sort(trip);
+                        ans.add(trip);
+                    }else{
+                        if(!inds.contains(i) && !inds.contains(j)){
+                            trip.add(nums[i]);
+                            trip.add(nums[j]);
+                            trip.add(nums[inds.get(0)]);
+                            Collections.sort(trip);
+                            ans.add(trip);
+                        }
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(ans);
+    }
+}
