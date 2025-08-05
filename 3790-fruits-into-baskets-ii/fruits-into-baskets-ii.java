63772810e69e5f4a23df35e7ebdaff96861247c8
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2025-08-06 02:30:34 +0530

    Time: 2 ms (61.07%) | Memory: 44.6 MB (46.44%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
@@ -1,15 +1,15 @@
 class Solution {
 
     public int numOfUnplacedFruits(int[] fr, int[] bk) {
-        Set<Integer> set = new HashSet<>();
+        boolean[] used = new boolean[100+1];
         int ans = fr.length;
         for(int i = 0 ; i < fr.length; i++){
             for(int j = 0; j<fr.length; j++){
-                if(set.contains(j))
+                if(used[j])
                     continue;
                 if(bk[j] >= fr[i]){
                     ans--;
-                    set.add(j);
+                    used[j] = true;
                     break;
                 }
             }
