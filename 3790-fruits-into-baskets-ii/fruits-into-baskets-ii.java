cfeca8964d3020b3df1797e71fcc571b063d4b2e
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2025-08-06 02:29:12 +0530

    Time: 10 ms (5.25%) | Memory: 44.8 MB (15.38%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
@@ -1,9 +1,6 @@
 class Solution {
 
     public int numOfUnplacedFruits(int[] fr, int[] bk) {
-        //1.fr can only placed from left -> right
-        //2.can contain only one type
-        // what we need to find first max from left.
         Set<Integer> set = new HashSet<>();
         int ans = fr.length;
         for(int i = 0 ; i < fr.length; i++){
