627a036fc4a8b48433cdf89c3be771fc5486e334
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2025-11-02 14:45:06 +0530

    Time: 2 ms (43.54%) | Memory: 45 MB (5.82%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,22 +1,24 @@
 class Solution {
     List<String> ans = new ArrayList<>();
-    int max;
-    void generate(int open,int close, String cur){
-        if(open == max && close == max){
-            ans.add(cur);
+    void generate(int open, int close,int n ,String str){
+        if(open > n || close > n)
+            return;
+        if(open == close && open == n){
+            ans.add(str);
             return;
         }
-        if(open < max){
-            generate(open+1 , close,cur + "(");
-        }
-        if(close < open){
-            generate(open , close + 1,cur + ")");
-        }
+        //decision for adding open or close br
         
+        //1.case 1 just add open br
+        generate(open+1,close,n,str + "(");
+        //2.case 2 open br is grater then close br then add close br
+        if(open > close){
+            generate(open,close+1,n,str + ")");
+        }
     }
     public List<String> generateParenthesis(int n) {
-        max = n;
-        generate(0,0,"");
+        generate(0,0,n,"");
+
         return ans;
     }
 }
