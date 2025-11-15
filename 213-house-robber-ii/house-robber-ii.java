34d03b3e032139f8da5b66d9d3961c8a76f88c40
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2025-11-15 19:15:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (16.94%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    int[] dp;
+    int dfs(int ind,int[] nums,int max) {
+        if(ind >= max){
+            return 0;
+        }
+        if(dp[ind] != -1)
+            return dp[ind];
+        //rob
+        int rob = nums[ind] + dfs(ind+2,nums,max);
+        int not = dfs(ind+1,nums,max);
+        dp[ind] = Math.max(rob,not);
+        return dp[ind];
+    }
+    public int rob(int[] nums) {
+        if(nums.length == 1)
+            return nums[0];
+        dp = new int[101];
+        Arrays.fill(dp,-1);
+        int r1 = dfs(1,nums,nums.length);
+        
+        Arrays.fill(dp,-1);
+        int r2 = dfs(0,nums,nums.length-1);
+        
+        return r1 > r2 ? r1 : r2;
+
+    }
+}
