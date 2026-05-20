a9f2114e2afa54707eba3d62252c176575cb7baf
Robin kumar <72180986+RobinKumar5986@users.noreply.github.com>
2026-05-21 01:53:51 +0530

    Time: 1 ms (100.00%) | Memory: 47.3 MB (69.94%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
@@ -1,25 +1,22 @@
 class Solution {
-    public int[] findThePrefixCommonArray(int[] a, int[] b) {
-        int[] ans = new int[a.length];
-        Set<Integer> set = new HashSet<>();
+    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+        int[] m1 = new int[A.length+1];
+        int[] m2 = new int[A.length+1];
+
+        int ans[] = new int[A.length];
         int co = 0;
-        for(int i = 0;i<a.length;i++){
-            int e1 = a[i];
-            int e2 = b[i];
-            if(e1 == e2){
-                co++;
+        // the good thing about this question is the nubers wont repeat
+        for(int i = 0; i<A.length;i++){
+            m1[A[i]]++;
+            m2[B[i]]++;
+            if(A[i] == B[i]){
+                co += m1[B[i]];
             }else{
-                if(set.contains(e1)){
-                    co++;
-                }
-                if(set.contains(e2)){
-                    co++;
-                }
+                co += m1[B[i]];
+                co += m2[A[i]];
             }
-            set.add(e1);
-            set.add(e2);
             ans[i] = co;
-        }
+        } 
         return ans;
     }
 }
